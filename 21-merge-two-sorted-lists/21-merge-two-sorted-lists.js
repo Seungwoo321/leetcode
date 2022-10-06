bc9dcd4103cdcf7aa971d90069f4dd5d37d73ec0
Seungwoo321 <seungwoo321@gmail.com>
2022-10-06 18:49:40 +0900

    Time: 76 ms (89.40%), Space: 43.9 MB (77.58%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -10,10 +10,6 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
-function ListNode(val, next) {
-    this.val = (val === undefined ? 0 : val)
-    this.next = (next === undefined ? null : next)
-}
 var mergeTwoLists = function (list1, list2) {
     let node = new ListNode()
     let head = node
