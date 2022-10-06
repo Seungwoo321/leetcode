609f9ad99a083d99246a6347ecc368407618b8bb
Seungwoo321 <seungwoo321@gmail.com>
2022-10-06 18:57:22 +0900

    Time: 72 ms (83.88%), Space: 41.6 MB (97.25%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+    let i = m + n - 1;
+    let p1 = m - 1;
+    let p2 = n - 1;
+    while (p2 >= 0) {
+        if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+            nums1[i--] = nums1[p1--];
+        } else {
+            nums1[i--] = nums2[p2--];
+        }
+    }
+};
