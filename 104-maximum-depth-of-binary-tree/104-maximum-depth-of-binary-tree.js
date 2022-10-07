14bb69d166338485b62b58c92d7a14c3932ce08c
Seungwoo321 <seungwoo321@gmail.com>
2022-10-07 16:35:30 +0900

    Time: 85 ms (81.29%), Space: 44.9 MB (81.54%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -10,7 +10,27 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var maxDepth = function(root) {
+// var maxDepth = function(root) {
+//     if (!root) return 0;
+//     return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+// };
+
+var maxDepth = function (root) {
     if (!root) return 0;
-    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    let level = 0;
+    const queue = [root];
+    while (queue.length) {
+        const len = queue.length
+        for (let i = 0; i < len; i ++) {
+            const node = queue.pop();
+            if (node && node.left) {
+                queue.unshift(node.left);
+            }
+            if (node && node.right) {
+                queue.unshift(node.right);
+            }
+        }
+        level ++;
+    }
+    return level;
 };
