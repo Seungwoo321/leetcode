c25d21a496a86e0866432eea8c550c21b6b3ef38
Seungwoo321 <seungwoo321@gmail.com>
2022-10-07 19:16:07 +0900

    Time: 78 ms (93.52%), Space: 44.6 MB (52.03%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.js
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.js
@@ -11,12 +11,7 @@
  * @return {TreeNode}
  */
 var sortedArrayToBST = function(nums, start = 0, end = nums.length - 1) {
-    if (start > end) {
-        return null;
-    }
+    if (start > end) return null;
     const mid = parseInt((start + end) / 2);
-    const node = new TreeNode(nums[mid]);
-    node.left = sortedArrayToBST(nums, start, mid - 1);
-    node.right = sortedArrayToBST(nums, mid + 1, end);
-    return node;
+    return new TreeNode(nums[mid], sortedArrayToBST(nums, start, mid - 1), sortedArrayToBST(nums, mid + 1, end));
 };
