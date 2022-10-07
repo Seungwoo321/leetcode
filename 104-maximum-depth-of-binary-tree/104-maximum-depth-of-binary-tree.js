10d7c425a7994fdfa6062a4bfe52bb0d23da1e27
Seungwoo321 <seungwoo321@gmail.com>
2022-10-07 16:36:12 +0900

    Time: 144 ms (5.22%), Space: 46.5 MB (5.72%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -15,22 +15,35 @@
 //     return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
 // };
 
+// var maxDepth = function (root) {
+//     if (!root) return 0;
+//     let level = 0;
+//     const queue = [root];
+//     while (queue.length) {
+//         const len = queue.length
+//         for (let i = 0; i < len; i ++) {
+//             const node = queue.pop();
+//             if (node && node.left) {
+//                 queue.unshift(node.left);
+//             }
+//             if (node && node.right) {
+//                 queue.unshift(node.right);
+//             }
+//         }
+//         level ++;
+//     }
+//     return level;
+// };
 var maxDepth = function (root) {
-    if (!root) return 0;
-    let level = 0;
-    const queue = [root];
-    while (queue.length) {
-        const len = queue.length
-        for (let i = 0; i < len; i ++) {
-            const node = queue.pop();
-            if (node && node.left) {
-                queue.unshift(node.left);
-            }
-            if (node && node.right) {
-                queue.unshift(node.right);
-            }
+    const stack = [[root, 1]];
+    let maxDepth = 0;
+    while (stack.length) {
+        const [node, depth] = stack.shift();
+        if (node) {
+            maxDepth = Math.max(depth, maxDepth);
+            stack.unshift([node.left, depth + 1 ]);
+            stack.unshift([node.right, depth + 1]);
         }
-        level ++;
     }
-    return level;
+    return maxDepth;
 };
