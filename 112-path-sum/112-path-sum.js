719a8a31d536ff4ef049c99246fbf72ef688fc1c
Seungwoo321 <seungwoo321@gmail.com>
2022-10-08 17:39:44 +0900

    Time: 122 ms (36.18%), Space: 46 MB (22.10%) - LeetHub

--- 112-path-sum/112-path-sum.js
+++ 112-path-sum/112-path-sum.js
@@ -13,6 +13,23 @@
  */
 var hasPathSum = function (root, targetSum) {
     if (!root) return false;
-    if (root.left === null && root.right === null && root.val === targetSum) return true;
-    return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    const stack = [[root, root.val]];
+    while (stack.length) {
+        const [node, val] = stack.shift();
+        if (node.left) {
+            stack.unshift([node.left, val + node.left.val]);
+        }
+        if (node.right) {
+            stack.unshift([node.right, val + node.right.val]);
+        }
+        if (!node.left && !node.right) {
+            if (val === targetSum) return true;
+        }
+    }
+    return false;
 };
+// var hasPathSum = function (root, targetSum) {
+//     if (!root) return false;
+//     if (root.left === null && root.right === null && root.val === targetSum) return true;
+//     return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+// };
