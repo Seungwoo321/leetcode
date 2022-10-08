71b765f7471d4dc4c87a49edc30580cff90f2b5d
Seungwoo321 <seungwoo321@gmail.com>
2022-10-08 19:27:50 +0900

    Time: 98 ms (36.46%), Space: 41.4 MB (97.16%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.js
+++ 118-pascals-triangle/118-pascals-triangle.js
@@ -2,15 +2,14 @@
  * @param {number} numRows
  * @return {number[][]}
  */
-var generate = function(numRows) {
-    const result = [];
-    for (let i = 0; i < numRows; i ++) {
-        let row = [];
-        row.push(1);
+var generate = function (numRows) {
+    const result = [[1]];
+    for (let i = 1; i < numRows; i ++) {
+        let row = [1];
         for (let j = 0; j < i; j++) {
             row.push(result[i - 1][j] + (result[i - 1][j + 1] || 0));
         } 
         result.push(row);
     }
     return result;
-}; 
+};
