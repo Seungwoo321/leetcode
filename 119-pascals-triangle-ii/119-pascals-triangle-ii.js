9a44fac07a1dcf4b703ae9e150d7a62f89535d58
Seungwoo321 <seungwoo321@gmail.com>
2022-10-08 20:47:22 +0900

    Time: 82 ms (72.95%), Space: 42 MB (71.38%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.js
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.js
@@ -2,20 +2,32 @@
  * @param {number} rowIndex
  * @return {number[]}
  */
+// var getRow = function(rowIndex) {
+//     const getTriangle = (rowIndex) => {
+//         if (rowIndex === 0) return []
+//         if (rowIndex === 1) return [[1]]
+
+//         const triangle = getTriangle(rowIndex - 1);
+//         const row = [1]
+
+//         for (let i = 0; i < rowIndex - 2; i++) {
+//             row.push(triangle[rowIndex - 2][i] + (triangle[rowIndex - 2][i + 1] || 0));
+//         }
+//         row.push(1);
+//         triangle.push(row);
+//         return triangle;
+//     }
+//     return getTriangle(rowIndex + 1)[rowIndex];
+// };
 var getRow = function(rowIndex) {
-    const getTriangle = (rowIndex) => {
-        if (rowIndex === 0) return []
-        if (rowIndex === 1) return [[1]]
-
-        const triangle = getTriangle(rowIndex - 1);
-        const row = [1]
-
-        for (let i = 0; i < rowIndex - 2; i++) {
-            row.push(triangle[rowIndex - 2][i] + (triangle[rowIndex - 2][i + 1] || 0));
+    const triangle = [[1]];
+    for (let i = 1; i < rowIndex + 1; i ++) {
+        const row = [1];
+        const prevRow = triangle[i - 1];
+        for (let j = 0; j < i; j ++) {
+            row.push(prevRow[j] + (prevRow[j + 1] || 0));
         }
-        row.push(1);
         triangle.push(row);
-        return triangle;
     }
-    return getTriangle(rowIndex + 1)[rowIndex];
+    return triangle[rowIndex];
 };
