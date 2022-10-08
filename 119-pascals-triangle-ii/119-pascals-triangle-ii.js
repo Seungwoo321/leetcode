0fb64f8794a408f4fc0a0615b0b2591c388a0849
Seungwoo321 <seungwoo321@gmail.com>
2022-10-08 20:46:58 +0900

    Time: 111 ms (19.20%), Space: 42.2 MB (50.00%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.js
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.js
@@ -3,14 +3,19 @@
  * @return {number[]}
  */
 var getRow = function(rowIndex) {
-    const triangle = [[1]];
-    for (let i = 1; i < rowIndex + 1; i ++) {
-        const row = [1];
-        const prevRow = triangle[i - 1];
-        for (let j = 0; j < i; j ++) {
-            row.push(prevRow[j] + (prevRow[j + 1] || 0));
+    const getTriangle = (rowIndex) => {
+        if (rowIndex === 0) return []
+        if (rowIndex === 1) return [[1]]
+
+        const triangle = getTriangle(rowIndex - 1);
+        const row = [1]
+
+        for (let i = 0; i < rowIndex - 2; i++) {
+            row.push(triangle[rowIndex - 2][i] + (triangle[rowIndex - 2][i + 1] || 0));
         }
+        row.push(1);
         triangle.push(row);
+        return triangle;
     }
-    return triangle[rowIndex];
+    return getTriangle(rowIndex + 1)[rowIndex];
 };
