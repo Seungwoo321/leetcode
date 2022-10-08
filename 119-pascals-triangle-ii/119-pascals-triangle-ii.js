35423af5eef76f0823e5d4d83b0e737b94f8dc05
Seungwoo321 <seungwoo321@gmail.com>
2022-10-08 20:54:55 +0900

    Time: 122 ms (8.70%), Space: 41.9 MB (71.38%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.js
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.js
@@ -2,32 +2,13 @@
  * @param {number} rowIndex
  * @return {number[]}
  */
-// var getRow = function(rowIndex) {
-//     const getTriangle = (rowIndex) => {
-//         if (rowIndex === 0) return []
-//         if (rowIndex === 1) return [[1]]
-
-//         const triangle = getTriangle(rowIndex - 1);
-//         const row = [1]
-
-//         for (let i = 0; i < rowIndex - 2; i++) {
-//             row.push(triangle[rowIndex - 2][i] + (triangle[rowIndex - 2][i + 1] || 0));
-//         }
-//         row.push(1);
-//         triangle.push(row);
-//         return triangle;
-//     }
-//     return getTriangle(rowIndex + 1)[rowIndex];
-// };
-var getRow = function(rowIndex) {
-    const triangle = [[1]];
+var getRow = function (rowIndex) {
+    const row = [1];
     for (let i = 1; i < rowIndex + 1; i ++) {
-        const row = [1];
-        const prevRow = triangle[i - 1];
-        for (let j = 0; j < i; j ++) {
-            row.push(prevRow[j] + (prevRow[j + 1] || 0));
+        for (let j = row.length - 1; j > 0; j --) {
+            row[j] = row[j - 1] + row[j];
         }
-        triangle.push(row);
+        row.push(1);
     }
-    return triangle[rowIndex];
+    return row;
 };
