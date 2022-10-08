ad8d1f199dff2720267ce39f6ad855ff5833147c
Seungwoo321 <seungwoo321@gmail.com>
2022-10-08 17:04:12 +0900

    Time: 370 ms (46.63%), Space: 97.7 MB (76.19%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.js
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.js
@@ -17,21 +17,37 @@
 //     if (root.right == null) return 1 + minDepth(root.left);
 //     return 1 + Math.min(minDepth(root.left), minDepth(root.right));
 // };
+// var minDepth = function (root) {
+//     if (!root) return 0;
+//     const stack = [[root, 1]];
+//     let depth = [];
+//     while (stack.length) {
+//         const [node, level] = stack.shift();
+//         if (node.left) {
+//             stack.unshift([node.left, level + 1]);
+//         }
+//         if (node.right) {
+//             stack.unshift([node.right, level + 1])
+//         }
+//         if (node && (!node.left && !node.right)) {
+//             depth.push(level);
+//         } 
+//     }
+//     return Math.min.apply(null, depth);
+// };
 var minDepth = function (root) {
     if (!root) return 0;
-    const stack = [[root, 1]];
-    let depth = [];
-    while (stack.length) {
-        const [node, level] = stack.shift();
+    const q = [[root, 1]];
+    while (q.length) {
+        const [node, level] = q.shift();
         if (node.left) {
-            stack.unshift([node.left, level + 1]);
+            q.push([node.left, level + 1]);
         }
         if (node.right) {
-            stack.unshift([node.right, level + 1])
+            q.push([node.right, level + 1]);
         }
         if (node && (!node.left && !node.right)) {
-            depth.push(level);
-        } 
+            return level;
+        }
     }
-    return Math.min.apply(null, depth);
 };
