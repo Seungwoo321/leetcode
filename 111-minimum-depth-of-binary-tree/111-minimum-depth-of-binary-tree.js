f821555f3340f2d26ed53278fb793cc843a90951
Seungwoo321 <seungwoo321@gmail.com>
2022-10-08 17:17:20 +0900

    Time: 275 ms (82.21%), Space: 83.4 MB (90.84%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.js
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.js
@@ -10,13 +10,12 @@
  * @param {TreeNode} root
  * @return {number}
  */
-// var minDepth = function (root) {
-//     if (!root) return 0;
-//     if (root.left === null && root.right === null) return 1;
-//     if (root.left === null) return 1 + minDepth(root.right);
-//     if (root.right == null) return 1 + minDepth(root.left);
-//     return 1 + Math.min(minDepth(root.left), minDepth(root.right));
-// };
+var minDepth = function (root) {
+    if (!root) return 0;
+    if (root.left === null) return 1 + minDepth(root.right);
+    if (root.right === null) return 1 + minDepth(root.left);
+    return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+};
 // var minDepth = function (root) {
 //     if (!root) return 0;
 //     const stack = [[root, 1]];
@@ -35,19 +34,19 @@
 //     }
 //     return Math.min.apply(null, depth);
 // };
-var minDepth = function (root) {
-    if (!root) return 0;
-    const q = [[root, 1]];
-    while (q.length) {
-        const [node, level] = q.shift();
-        if (node.left) {
-            q.push([node.left, level + 1]);
-        }
-        if (node.right) {
-            q.push([node.right, level + 1]);
-        }
-        if (node && (!node.left && !node.right)) {
-            return level;
-        }
-    }
-};
+// var minDepth = function (root) {
+//     if (!root) return 0;
+//     const q = [[root, 1]];
+//     while (q.length) {
+//         const [node, level] = q.shift();
+//         if (node.left) {
+//             q.push([node.left, level + 1]);
+//         }
+//         if (node.right) {
+//             q.push([node.right, level + 1]);
+//         }
+//         if (node && (!node.left && !node.right)) {
+//             return level;
+//         }
+//     }
+// };
