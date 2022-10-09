3b9765f66a2ec3a6df1c816a6faa9bfa1ac4ab18
Seungwoo321 <seungwoo321@gmail.com>
2022-10-09 13:20:03 +0900

    Time: 159 ms (11.98%), Space: 46.6 MB (41.19%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -4,8 +4,5 @@
  */
 var isPalindrome = function(s) {
     const str = s.replace(/[^A-Za-z0-9]/g, '').toLocaleLowerCase();
-    const len = parseInt(str.length / 2);
-    return Array.prototype.every.call(str.substring(0, len), (v, i) => {
-        return v === str[str.length - 1 - i];
-    })
+    return str === str.split('').reverse().join('');
 };
