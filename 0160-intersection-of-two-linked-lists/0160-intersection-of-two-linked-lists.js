9af397d5ca7bf4e23cf4d4e972296c8f03e10ef2
Seungwoo321 <seungwoo321@gmail.com>
2022-11-07 00:11:35 +0900

    Time: 164 ms (33.03%), Space: 50.1 MB (54.73%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
@@ -12,11 +12,11 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function(headA, headB) {
-    let nodeA = headA;
-    let nodeB = headB;
-    while (nodeA !== nodeB) {
-        nodeA = nodeA ? nodeA.next : headB;
-        nodeB = nodeB ? nodeB.next : headA;
+    let p1 = headA;
+    let p2 = headB;
+    while (p1 !== p2) {
+        p1 = p1 ? p1.next : headB;
+        p2 = p2 ? p2.next : headA;
     }
-    return nodeA;
+    return p1;
 };
