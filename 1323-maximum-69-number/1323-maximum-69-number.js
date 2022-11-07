4162366f02e9d4f0513009b660fcebbec59b2a6f
Seungwoo321 <seungwoo321@gmail.com>
2022-11-07 23:12:26 +0900

    Time: 84 ms (75.62%), Space: 42.6 MB (5.19%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.js
+++ 1323-maximum-69-number/1323-maximum-69-number.js
@@ -5,8 +5,8 @@
 var maximum69Number  = function(num) {
     for (let i = 3; i > -1; i --) {
         if (parseInt(num / (10 ** i) % 10) === 6) {
-            return num + 3 * (10 ** i)
+            return num + 3 * (10 ** i);
         }
     }
-    return num
+    return num;
 };
