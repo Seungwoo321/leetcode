fb889fab7c0572024bb1fd0b378f8dc0bc08ceac
Seungwoo321 <seungwoo321@gmail.com>
2022-11-08 23:51:32 +0900

    Time: 104 ms (21.74%), Space: 41.9 MB (42.09%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.js
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.js
@@ -4,7 +4,7 @@
  */
 var convertToTitle = function(columnNumber) {
     let column = '';
-    while (columnNumber != 0) {
+    while (columnNumber > 0) {
         columnNumber -= 1;
         column = String.fromCharCode((columnNumber % 26) + 65) + column;
         columnNumber = parseInt(columnNumber / 26);
