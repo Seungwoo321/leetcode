d94f64ef0d5be51e4b7f3754eff605be8065f669
Seungwoo321 <seungwoo321@gmail.com>
2022-11-09 09:44:20 +0900

    Time: 111 ms (68.31%), Space: 44 MB (38.52%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} columnTitle
+ * @return {number}
+ */
+var titleToNumber = function(columnTitle) {    
+    return [...columnTitle].reduce((acc, cur, i) => {
+        return acc + (cur.charCodeAt() - 65 + 1) * 26 ** (columnTitle.length -1 - i);
+    }, 0)
+};
