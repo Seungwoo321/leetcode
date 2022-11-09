d4808dbfd0c6de9034a2d15a9d2aefc2001e8c5e
Seungwoo321 <seungwoo321@gmail.com>
2022-11-09 23:25:56 +0900

    Time: 106 ms (60.11%), Space: 43.5 MB (64.40%) - LeetHub

--- 0202-happy-number/0202-happy-number.js
+++ 0202-happy-number/0202-happy-number.js
@@ -6,11 +6,11 @@ var isHappy = function(n) {
     let p1 = n;
     let p2 = n;
     const sum = (acc, cur) => acc + +cur * +cur;
-    while (true) {
+    while (n > 1) {
         p1 = [...p1.toString()].reduce(sum, 0);
         p2 = [...[...p2.toString()].reduce(sum, 0).toString()].reduce(sum, 0);
-        if (p1 === 1) return true
-        if (p1 === p2) return false
+        if (p1 === 1) return true;
+        if (p1 === p2) return false;
     }
-    return false
+    return true;
 };
