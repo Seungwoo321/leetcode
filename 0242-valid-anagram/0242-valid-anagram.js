ffa311dece75352ee8bb92bae02b845de3b34a6a
Seungwoo321 <seungwoo321@gmail.com>
2022-11-11 23:06:34 +0900

    Time: 107 ms (83.05%), Space: 43.4 MB (72.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,23 +4,23 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    return s.length === t.length && [...s].sort().join('') === [...t].sort().join('');
+    // return s.length === t.length && [...s].sort().join('') === [...t].sort().join('');
     
-    // if (s.length !== t.length) {
-    //     return false;
-    // }
-    // return Object.values([...t].reduce((acc, cur) => {
-    //     if (acc[cur]) {
-    //         acc[cur]--;
-    //     } else {
-    //         acc[cur] = acc[cur] ? acc[cur] + 1 : 1;
-    //     }
-    //     if (acc[cur] === 0) {
-    //         delete acc[cur];
-    //     }
-    //     return acc;
-    // },[...s].reduce((acc, cur) => {
-    //     acc[cur] = acc[cur] ? acc[cur] + 1 : 1
-    //     return acc;
-    // }, {}))).length === 0;
+    if (s.length !== t.length) {
+        return false;
+    }
+    return Object.values([...t].reduce((acc, cur) => {
+        if (acc[cur]) {
+            acc[cur]--;
+        } else {
+            acc[cur] = acc[cur] ? acc[cur] + 1 : 1;
+        }
+        if (acc[cur] === 0) {
+            delete acc[cur];
+        }
+        return acc;
+    },[...s].reduce((acc, cur) => {
+        acc[cur] = acc[cur] ? acc[cur] + 1 : 1
+        return acc;
+    }, {}))).length === 0;
 };
