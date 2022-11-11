d577f09ea0c98741bbb9cd2adac382278200c886
Seungwoo321 <seungwoo321@gmail.com>
2022-11-11 23:48:01 +0900

    Time: 84 ms (94.98%), Space: 42.2 MB (97.60%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -10,20 +10,18 @@ var isAnagram = function(s, t) {
         return false;
     }
     let sMap = {};
+    let tMap = {};
     for (let i = 0; i < s.length; i ++) {
         sMap[s[i]] = (sMap[s[i]] || 0) + 1;
+        tMap[t[i]] = (tMap[t[i]] || 0) + 1;
     }
-    for (let i = 0; i < t.length; i ++) {
-        if (sMap[t[i]]) {
-            sMap[t[i]]--;
-        } else {
-            sMap[t[i]] = (sMap[t[i]] || 0) + 1;
-        }
-        if (sMap[t[i]] === 0) {
-            delete sMap[t[i]];
+    for (let i = 0; i < s.length; i ++) {
+        if (sMap[s[i]] !== tMap[s[i]]) {
+            return false
         }
     }
-    return Object.values(sMap).length === 0;
+    
+    return true;
     // return Object.values([...t].reduce((acc, cur) => {
     //     if (acc[cur]) {
     //         acc[cur]--;
