f62ca84002c33245319b9f8ebf04d475d6619fd5
Seungwoo321 <seungwoo321@gmail.com>
2022-11-12 21:19:42 +0900

    Time: 215 ms (50.83%), Space: 64.1 MB (44.08%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    const hashTable = {}
+    for (let i = 0; i < nums.length; i ++) {
+
+        if (typeof hashTable[nums[i]] !== 'number') {
+            hashTable[nums[i]] = i;
+        } else {
+            if (Math.abs(hashTable[nums[i]] - i) <= k) {
+                return true;
+            }
+            hashTable[nums[i]] = i;
+        }
+    }
+    return false;
+};
