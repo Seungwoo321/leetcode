3ed39fa811db68305c1e9ef9e68b6e68ace413f2
Seungwoo321 <seungwoo321@gmail.com>
2022-11-12 19:27:39 +0900

    Time: 227 ms (49.84%), Space: 54.2 MB (36.91%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -3,10 +3,9 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    const hashTable = strs.reduce((table, str) => {
+    return Object.values(strs.reduce((table, str) => {
         const key = [...str].sort().join('');
         table[key] = [str, ...table[key] ?? []];
         return table;
-    }, {});
-    return Object.values(hashTable);
+    }, {}));
 };
