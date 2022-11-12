46e79ab38b880bf2a1fcadfb75aba552c9e06d54
Seungwoo321 <seungwoo321@gmail.com>
2022-11-12 22:11:29 +0900

    Time: 91 ms (58.52%), Space: 42.2 MB (24.33%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.js
@@ -0,0 +1,47 @@
+
+var MyStack = function() {
+    this.queue = [null, null]
+    this.size = 0;
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+    this.size ++;
+    this.queue[this.size] = x;
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+    const value = this.queue[this.size]
+    this.queue[this.size] = null;
+    this.size --;
+    return value
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+    return this.queue[this.size]
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+    return this.size === 0;
+};
+
+/** 
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
