6a055674efa95d2aaa5dd956e82c219f510247ef
Seungwoo321 <seungwoo321@gmail.com>
2022-11-14 19:58:32 +0900

    Time: 150 ms (26.29%), Space: 44.3 MB (61.70%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    nums = nums.sort((a, b) => a - b)
+    for (let i = 0; i < nums.length; i ++) {
+        if (i !== nums[i]) {
+            return i
+        }
+    }
+    return nums.length
+};
