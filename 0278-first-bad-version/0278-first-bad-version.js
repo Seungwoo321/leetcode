d43dc0ae145d02987354986b3848cb8a0ba68f4b
Seungwoo321 <seungwoo321@gmail.com>
2022-11-14 20:37:38 +0900

    Time: 94 ms (49.97%), Space: 41.9 MB (53.53%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        if (n === 1) return n;
+        let p1 = 1;
+        let p2 = n
+        
+        while (p1 < p2) {
+            let version = parseInt((p1 + p2) / 2)
+            if (isBadVersion(version)) {
+                if (isBadVersion(version - 1)) {
+                    p2 = version - 1;
+                } else {
+                    return version;
+                }
+            } else {
+                p1 = version + 1;
+            }
+        }
+        return p1
+        
+    };
+};
