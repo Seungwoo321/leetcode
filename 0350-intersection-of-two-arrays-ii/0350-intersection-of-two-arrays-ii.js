14d3da331de8099ee610ec79c7585d0bb66dbdca
Seungwoo321 <seungwoo321@gmail.com>
2022-11-16 20:18:22 +0900

    Time: 92 ms (74.90%), Space: 43.4 MB (55.30%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
@@ -9,7 +9,7 @@ var intersect = function(nums1, nums2) {
     
     const hashTable = nums1.reduce((acc, cur) => {
         acc[cur] ? acc[cur] += 1 : acc[cur] = 1;
-        return acc
+        return acc;
     }, {})
     return [...nums2.filter(n => hashTable[n] > 0 && hashTable[n]--)];
 };
