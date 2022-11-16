1eeed3f8a7cd6f7ad179a4159c45ac2b33ed294c
Seungwoo321 <seungwoo321@gmail.com>
2022-11-16 20:04:28 +0900

    Time: 69 ms (91.21%), Space: 43.6 MB (48.63%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -4,29 +4,31 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-    nums1 = nums1.sort((a, b) => a - b);
-    nums2 = nums2.sort((a, b) => a - b);
-    let p1 = 0;
-    let p2 = 0;
-    let added = null
-    let answer = [];
-    while (p1 < nums1.length || p2 < nums2.length) {
-        if (nums1[p1] > nums2[p2]) {
-            p2 ++;
-        } else if (nums1[p1] < nums2[p2]) {
-            p1 ++;
-        } else if (nums1[p1] === nums2[p2]){
-            if (added !== nums1[p1] ) {
-                answer.push(nums1[p1] );
-                added = nums1[p1] ;
-            }
-            p1 ++;
-            p2 ++;
-        } else {
-            p1 < nums1.length && p1 ++;
-            p2 < nums2.length && p2 ++;
-        }
-    }
-    return answer;
+    const set = new Set(nums1);
+    return [...new Set(nums2.filter(n => set.has(n)))];
+    // nums1 = nums1.sort((a, b) => a - b);
+    // nums2 = nums2.sort((a, b) => a - b);
+    // let p1 = 0;
+    // let p2 = 0;
+    // let added = null
+    // let answer = [];
+    // while (p1 < nums1.length || p2 < nums2.length) {
+    //     if (nums1[p1] > nums2[p2]) {
+    //         p2 ++;
+    //     } else if (nums1[p1] < nums2[p2]) {
+    //         p1 ++;
+    //     } else if (nums1[p1] === nums2[p2]){
+    //         if (added !== nums1[p1] ) {
+    //             answer.push(nums1[p1] );
+    //             added = nums1[p1] ;
+    //         }
+    //         p1 ++;
+    //         p2 ++;
+    //     } else {
+    //         p1 < nums1.length && p1 ++;
+    //         p2 < nums2.length && p2 ++;
+    //     }
+    // }
+    // return answer;
     
 };
