b4d6f086ac50f44d3e52f2e561caa68ef74fed18
Seungwoo321 <seungwoo321@gmail.com>
2022-11-16 23:31:34 +0900

    Time: 128 ms (69.76%), Space: 45.2 MB (51.79%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.js
+++ 0383-ransom-note/0383-ransom-note.js
@@ -6,11 +6,11 @@
 var canConstruct = function(ransomNote, magazine) {
     const hashTable = new Array(26).fill(0);
     for (let char of ransomNote) {
-        const index = char.charCodeAt() % 26;        
+        const index = char.charCodeAt() % 97;        
         hashTable[index] = hashTable[index] > 0 ? hashTable[index] + 1 : 1;
     }
     for (let char of magazine) {
-        const index = char.charCodeAt() % 26;
+        const index = char.charCodeAt() % 97;
         hashTable[index] = hashTable[index] > 0 ? hashTable[index] - 1 : 0;
     }
     return hashTable.every(v => v === 0);
