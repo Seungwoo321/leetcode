dc714938ec25c6542b0da5f0677eb6b1e7a62e24
Seungwoo321 <seungwoo321@gmail.com>
2022-11-17 00:22:28 +0900

    Time: 181 ms (50.91%), Space: 48.4 MB (25.35%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -3,12 +3,5 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {
-    const hashTable = [...s].reduce((acc, cur) => {
-        acc[cur] = acc[cur] > 0 ? acc[cur] + 1 : 1;
-        return acc;
-    },{});
-    for (let i = 0; i < s.length; i ++) {
-        if (hashTable[s[i]] === 1) return i;
-    }
-    return -1;
+    return [...s].findIndex(c => s.indexOf(c) === s.lastIndexOf(c));
 };
