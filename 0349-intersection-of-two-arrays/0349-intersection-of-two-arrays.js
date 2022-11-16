198d60c9e13724ff477e92c4852bc2cd6bec797d
Seungwoo321 <seungwoo321@gmail.com>
2022-11-16 19:25:12 +0900

    Time: 90 ms (71.44%), Space: 43.9 MB (32.02%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    let set1 = new Set(nums1);
+    let set2 = new Set(nums2);
+    const result = [];
+    for (let n of set1) {
+        if (set2.has(n)) {
+            result.push(n)
+        }
+    }
+    return result;
+    
+};
