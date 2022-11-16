f52d4ecf1e04bfd63a7202742377f93521f677ee
Seungwoo321 <seungwoo321@gmail.com>
2022-11-16 21:51:55 +0900

    Time: 86 ms (76.39%), Space: 44 MB (36.30%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.js
+++ 0257-binary-tree-paths/0257-binary-tree-paths.js
@@ -12,27 +12,9 @@
  */
 var binaryTreePaths = function(root) {
     const paths = []
-//     if (!root) return paths;
-    
-//     if (!root.left && !root.right) return [`${root.val}`];
-//     const left = binaryTreePaths(root.left).map(x => `${root.val}->${x}`);
-//     const right = binaryTreePaths(root.right).map(x => `${root.val}->${x}`);
-//     return [...left, ...right];
-    
-    const dfs = function (root, path = '', paths = []) {
-        if (!root) return paths;
-        path += `${path ? '->' : ''}${root.val}`;
-        if (!root.left && !root.right) {
-            paths.push(path);
-        }
-        if (root.left) {
-            dfs(root.left, path, paths);
-        }
-        if (root.right) {
-            dfs(root.right, path, paths);
-        }
-        return paths;
-    }
-    dfs(root, '', paths);
-    return paths;
+    if (!root) return paths;
+    if (!root.left && !root.right) return [`${root.val}`];
+    const left = binaryTreePaths(root.left).map(x => `${root.val}->${x}`);
+    const right = binaryTreePaths(root.right).map(x => `${root.val}->${x}`);
+    return [...left, ...right];
 };
