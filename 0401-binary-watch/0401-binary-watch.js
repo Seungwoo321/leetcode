d99adf63b7965404497998896bab86cfb2baed7b
Seungwoo321 <seungwoo321@gmail.com>
2022-11-17 21:10:06 +0900

    Time: 99 ms (60.00%), Space: 42.1 MB (84.55%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.js
+++ 0401-binary-watch/0401-binary-watch.js
@@ -4,10 +4,17 @@
  */
 var readBinaryWatch = function(turnedOn) {
     const time = [];
-    const dp = countBits(60);
+    const bitCount = (num) => {
+      let res = 0;
+      while (num) {
+        res += num & 1;
+        num >>= 1;
+      }
+      return res;
+    };
     for (let i = 0; i < 12; i ++) {
         for (let j = 0; j < 60; j ++) {
-            if (dp[i] + dp[j] === turnedOn) {
+            if (bitCount(i) + bitCount(j) === turnedOn) {
                 time.push(`${i}:${j < 10 ? '0' : ''}${j}`);
             }
         }
@@ -16,22 +23,3 @@ var readBinaryWatch = function(turnedOn) {
     
 };
 
-const bitCount = (num) => {
-  let res = 0;
-  while (num) {
-    res += num & 1;
-    num >>= 1;
-  }
-  return res;
-}
-var countBits = function(n) {
-    const dp = [0];
-    let offset = 1;
-    for (let i = 1; i <= n; i ++) {
-        if (offset * 2 === i) {
-            offset = i;
-        }
-        dp[i] = dp[i - offset] + 1;
-    }
-    return dp;
-};
