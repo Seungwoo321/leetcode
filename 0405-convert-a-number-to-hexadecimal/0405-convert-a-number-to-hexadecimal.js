8ddd4e21330375bafdf64d77c802326df86b47ab
Seungwoo321 <seungwoo321@gmail.com>
2022-11-17 22:30:55 +0900

    Time: 60 ms (97.98%), Space: 41.7 MB (88.66%) - LeetHub

---
+++ 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var toHex = function(num) {
+    if (num == 0) return '0'
+    if (num < 0) num += 2 ** 32;
+    let code = '0123456789abcdef';
+    let answer = '';
+    while (num > 0) {
+        let digit = num % 16;
+        answer = code[digit] + answer;
+        num = parseInt(num / 16);
+    }
+    return answer;
+};
