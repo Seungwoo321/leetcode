f80c30428a4e6b659282d8b6b77f466d2142e086
Seungwoo321 <seungwoo321@gmail.com>
2022-11-17 21:47:54 +0900

    Time: 110 ms (43.34%), Space: 44.5 MB (48.14%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.js
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.js
@@ -12,6 +12,22 @@
  */
 var sumOfLeftLeaves = function(root, isLeft = false) {
     if (!root) return 0;
-    if (!root.left && !root.right) return isLeft ? root.val : 0;
-    return sumOfLeftLeaves(root.left, true) + sumOfLeftLeaves(root.right, false);
+    // if (!root.left && !root.right) return isLeft ? root.val : 0;
+    // return sumOfLeftLeaves(root.left, true) + sumOfLeftLeaves(root.right, false);
+    
+    const queue = [[root, false]];
+    let sum = 0;
+    while (queue.length) {
+        const [node, isLeft] = queue.shift();
+        if (node.left) {
+            queue.push([node.left, true]);
+        }
+        if (node.right) {
+            queue.push([node.right, false]);
+        }
+        if (!node.left && !node.right) {
+            sum += isLeft ? node.val : 0;
+        }
+    }
+    return sum;
 };
