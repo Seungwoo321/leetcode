3a40a0f4e88ea9548c32461dab91b8eca67f76c0
Seungwoo321 <seungwoo321@gmail.com>
2022-11-17 23:49:32 +0900

    Time: 122 ms (21.97%), Space: 44.6 MB (30.24%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -3,15 +3,15 @@
  * @return {string[]}
  */
 var fizzBuzz = function(n) {
-    const answer = new Array(n).fill('');
+    const answer = new Array(n).fill(0);
     for (let i in answer) {
-        let num = +i +1;
+        let num = +i + 1;
         let fizz = num % 3 === 0 ? 'Fizz' : '';
         let buzz = num % 5 === 0 ? 'Buzz' : '';
         if (fizz || buzz) {
             answer[i] = fizz + buzz;
         } else {
-            answer[i] += num;
+            answer[i] = '' + num;
         }
         
     }
