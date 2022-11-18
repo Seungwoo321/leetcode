5482c1229fc9aa221a6aed155c53ed1b2c8b00ac
Seungwoo321 <seungwoo321@gmail.com>
2022-11-18 23:50:40 +0900

    Time: 84 ms (86.22%), Space: 44.2 MB (93.50%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.js
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.js
@@ -4,6 +4,6 @@
  */
 var repeatedSubstringPattern = function(s) {
     const ss = s + s;
-    const s2 = ss.substring(1, ss.length - 1);
-    return s2.includes(s);
+    const ss2 = ss.substring(1, ss.length - 1);
+    return ss2.includes(s);
 };
