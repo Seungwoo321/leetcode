3429a294b229870da959950a3bdcca81ae9734e6
Seungwoo321 <seungwoo321@gmail.com>
2022-11-18 23:02:55 +0900

    Time: 115 ms (52.02%), Space: 43.7 MB (41.64%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.js
+++ 0263-ugly-number/0263-ugly-number.js
@@ -3,11 +3,18 @@
  * @return {boolean}
  */
 var isUgly = function(n) {
-	  if (n <= 0) return false;
-		for (let num of [2, 3, 5]) {
-        while (n % num === 0) {
-            n = n / num;
+    if (n <= 0) return false;
+    while (n > 1) {
+        if (n % 2 === 0) {
+            n = n / 2;
+        } else if (n % 3 === 0) {
+            n = n / 3;
+        } else if (n % 5 === 0) {
+            n = n / 5;
+        } else {
+            return false;
         }
     }
-		return n === 1;
+    return n === 1;
 };
+
