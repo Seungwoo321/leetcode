b59ca78607bf5b2c2a81e055c17ad01bebf2646d
Seungwoo321 <seungwoo321@gmail.com>
2022-11-19 00:08:32 +0900

    Time: 55 ms (99.16%), Space: 41.8 MB (83.49%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,5 +4,6 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    return (haystack === needle || haystack.repeat(2).slice(1, -1).includes(needle) ? haystack.indexOf(needle) : -1);
+    // return (haystack === needle || haystack.repeat(2).slice(1, -1).includes(needle) ? haystack.indexOf(needle) : -1);
+    return haystack.indexOf(needle);
 };
