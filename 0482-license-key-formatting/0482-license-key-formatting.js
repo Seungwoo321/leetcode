5e33377a4cec56c39380e9bab6f367dcfb91ff9f
Seungwoo321 <seungwoo321@gmail.com>
2022-11-20 22:28:41 +0900

    Time: 117 ms (62.41%), Space: 47.4 MB (57.12%) - LeetHub

--- 0482-license-key-formatting/0482-license-key-formatting.js
+++ 0482-license-key-formatting/0482-license-key-formatting.js
@@ -5,10 +5,8 @@
  */
 var licenseKeyFormatting = function(s, k) {
     s = s.replaceAll('-', '').split('');
-    
     for (let i = s.length - k; i > 0; i -= k) {
         s[i] = `-${s[i]}`;
     }
-
     return s.join('').toUpperCase();
 };
