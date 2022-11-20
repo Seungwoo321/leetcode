26c60afe4c2f563948537d98900a9359d47cae4c
Seungwoo321 <seungwoo321@gmail.com>
2022-11-20 18:59:54 +0900

    Time: 527 ms (7.63%), Space: 56 MB (8.31%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var islandPerimeter = function(grid) {
+    const bfs = (y, x, visted) => {
+        if (visted[`${y},${x}`]) {
+            return 0;
+        }
+        if (!(y in grid) || !(x in grid[0]) || grid[y][x] === 0) {
+            return 1;
+        }
+        visted[`${y},${x}`] = true
+        return (bfs(y + 1, x, visted) 
+                + bfs(y - 1, x, visted)
+                + bfs(y, x + 1, visted)
+                + bfs(y, x - 1, visted))
+    }
+    for (let i = 0; i < grid.length; i ++) {
+        for (let j = 0; j < grid[i].length; j ++) {
+            if (grid[i][j] === 1) {
+                return bfs(i, j, {})
+            }
+        }
+    }
+};
