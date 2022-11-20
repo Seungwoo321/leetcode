f5960ceb95720f73905ead9266d135194a3608ad
Seungwoo321 <seungwoo321@gmail.com>
2022-11-20 21:56:27 +0900

    Time: 130 ms (43.98%), Space: 47.3 MB (61.86%) - LeetHub

--- 0482-license-key-formatting/0482-license-key-formatting.js
+++ 0482-license-key-formatting/0482-license-key-formatting.js
@@ -7,13 +7,11 @@ var licenseKeyFormatting = function(s, k) {
     let key = '';
     const str = s.replaceAll('-', '');
     let point = str.length % k;
-    
     for (let i = 0; i < str.length; i ++) {
         if (i > 0 && i % k === point) {
             key += '-';
         } 
         key += str[i];
-
     }
     return key.toUpperCase();
     
