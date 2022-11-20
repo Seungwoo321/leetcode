9a2bb642740e30e57a2d65350b72eac6df4d18df
Seungwoo321 <seungwoo321@gmail.com>
2022-11-20 20:42:38 +0900

    Time: 102 ms (33.02%), Space: 42 MB (56.07%) - LeetHub

--- 0476-number-complement/0476-number-complement.js
+++ 0476-number-complement/0476-number-complement.js
@@ -3,9 +3,9 @@
  * @return {number}
  */
 var findComplement = function(num) {
-    let d = 2;
-    while (d <= num) {
-        d *= 2;
-    }    
-    return d - num - 1;
+    let mask = 1;
+    while (mask < num) {
+        mask = (mask << 1) | 1;
+    }
+    return num ^ mask;
 };
