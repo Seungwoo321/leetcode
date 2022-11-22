26edd5a2d128bca88b133153370e4c539de2018f
Seungwoo321 <seungwoo321@gmail.com>
2022-11-22 22:50:18 +0900

    Time: 84 ms (91.72%), Space: 48.1 MB (45.36%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.js
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.js
@@ -11,22 +11,21 @@
  * @return {number}
  */
 var getMinimumDifference = function(root) {
+
     const inorder = (node, arr) => {
         if (node.left && node.left.val !== null) {
             inorder(node.left, arr);
         }
         if (node.val !== null) {
+            diff = arr.length ? Math.min(Math.abs(arr[arr.length - 1] - node.val), diff) : diff;
             arr.push(node.val);
         }
         if (node.right && node.right.val !== null) {
             inorder(node.right, arr);
         }
-        return arr;
     }
-    const arr = inorder(root, []);
+    
     let diff = Infinity;
-    for (let i = 1; i < arr.length; i ++) {
-        diff = Math.min(Math.abs(arr[i] - arr[i - 1]), diff);
-    }
+    inorder(root, []);
     return diff;
 };
