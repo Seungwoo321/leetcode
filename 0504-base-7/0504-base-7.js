5f7c4cb67f4d1b7900adca1c266367007548bc4b
Seungwoo321 <seungwoo321@gmail.com>
2022-11-22 16:34:09 +0900

    Time: 112 ms (11.38%), Space: 41.8 MB (84.55%) - LeetHub

--- 0504-base-7/0504-base-7.js
+++ 0504-base-7/0504-base-7.js
@@ -4,11 +4,11 @@
  */
 var convertToBase7 = function(num) {
     let sign = num < 0 ? '-' : '';
-    num = num * (sign + 1);
+    num = Math.abs(num)
     let result = '';
     while (num) {
         result = num % 7 + result;
-        num = num / 7 ^ 0;
+        num = num / 7 ^ 0
     }
     return sign + result || '0';
 };
