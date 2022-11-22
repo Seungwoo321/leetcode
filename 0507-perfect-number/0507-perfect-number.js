0460e7fee456a0070b3913a13e0ca2b15b465e18
Seungwoo321 <seungwoo321@gmail.com>
2022-11-22 19:12:43 +0900

    Time: 102 ms (86.59%), Space: 42.1 MB (51.59%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.js
+++ 0507-perfect-number/0507-perfect-number.js
@@ -4,13 +4,13 @@
  */
 var checkPerfectNumber = function(num) {
     let sum = 0;
-    for (let i = 1; i <= num / 2; i ++)  {
+    for (let i = 1; i * i <= num; i ++)  {
         if (num % i === 0) {
             sum += i;
-        }
-        if (sum > num) {
-            return false;
+            if (i * i !== num) {
+                sum += num / i;
+            }
         }
     }
-    return sum === num;
+    return sum - num === num;
 };
