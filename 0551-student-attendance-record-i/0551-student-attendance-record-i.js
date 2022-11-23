cbbc3448fa9bbeedcd6599413975cc08f654878a
Seungwoo321 <seungwoo321@gmail.com>
2022-11-23 23:01:14 +0900

    Time: 64 ms (92.05%), Space: 41.9 MB (81.82%) - LeetHub

---
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkRecord = function(s) {
+    return (s.indexOf('A') === -1 || (s.indexOf('A') === s.lastIndexOf('A'))) &&
+        (s.lastIndexOf('LLL') === -1)
+};
