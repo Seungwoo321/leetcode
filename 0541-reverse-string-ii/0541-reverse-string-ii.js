730e7c82900aa72c6de775c9140c916a3b8eb85b
Seungwoo321 <seungwoo321@gmail.com>
2022-11-23 10:40:00 +0900

    Time: 143 ms (5.20%), Space: 44.2 MB (76.95%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.js
+++ 0541-reverse-string-ii/0541-reverse-string-ii.js
@@ -5,9 +5,10 @@
  */
 var reverseStr = function(s, k) {
     let start = 0;
+    let reverseStr = [...s]
     let answer = '';
     while (start < s.length) {
-        answer += [...s.slice(start, start + k)].reverse().join('');
+        answer += reverseStr.slice(start, start + k).reverse().join('');
         let next = start + (2 * k)
         answer += s.slice(start + k, next);
         start = next;
