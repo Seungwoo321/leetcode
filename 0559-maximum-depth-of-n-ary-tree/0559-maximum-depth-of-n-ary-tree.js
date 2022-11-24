77065c3143e6b9a0c007c4d5fa7b5b5b715fe92c
Seungwoo321 <seungwoo321@gmail.com>
2022-11-24 16:12:59 +0900

    Time: 125 ms (43.61%), Space: 45.3 MB (35.51%) - LeetHub

---
+++ 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.js
@@ -0,0 +1,24 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if (!root) return 0;
+    const bfs = (root, depth) => {
+        if (!root.children) return depth;
+        let max = depth        
+        for (let node of root.children) {
+            max = Math.max(bfs(node, depth + 1), max);
+        }
+        return max;    
+    }
+    return bfs(root, 1);
+};
