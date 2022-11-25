7d8591a9f5fb5f737a3bbcf0f2828e75cd2f82cd
Seungwoo321 <seungwoo321@gmail.com>
2022-11-25 23:18:39 +0900

    Time: 125 ms (74.94%), Space: 49.1 MB (75.34%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.js
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.js
@@ -13,9 +13,9 @@
  */
 var isSubtree = function(root, subRoot) {
     if (!root) return root;
-
     return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot) || isSameTree(root, subRoot);
 };
+
 var isSameTree = function (p, q) {
     if (p === null || q === null) {
         return p === q;
