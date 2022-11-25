8b79a1c6b572f0d9d37c9fee43dcdf3feab0b87e
Seungwoo321 <seungwoo321@gmail.com>
2022-11-25 20:14:06 +0900

    Time: 133 ms (53.49%), Space: 46.1 MB (22.52%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.js
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.js
@@ -9,6 +9,7 @@ var matrixReshape = function(mat, r, c) {
     let n = mat[0].length;
     if (m * n !== r * c) return mat;
     let arr = new Array(r).fill(0).map(row => new Array(c).fill(0));
+    
     for (let i = 0; i < r * c; i ++) {
         arr[parseInt(i / c)][i % c] = mat[parseInt(i / n)][i % n];
     }
