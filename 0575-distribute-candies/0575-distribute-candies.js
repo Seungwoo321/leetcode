6eb2a4ca18c264845f9f88ed33f017a95a1e94ca
Seungwoo321 <seungwoo321@gmail.com>
2022-11-26 00:15:02 +0900

    Time: 200 ms (51.08%), Space: 56.4 MB (44.80%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} candyType
+ * @return {number}
+ */
+var distributeCandies = function(candyType) {
+    const set = new Set()
+    for (let i = 0; i < candyType.length; i ++) {
+        if (!set.has(candyType[i])) {
+            set.add(candyType[i]);
+        }
+    }
+    return Math.min(set.size, parseInt(candyType.length / 2));
+};
