4f48ce6265d9459f0825232558baf098d2c05a86
Seungwoo321 <seungwoo321@gmail.com>
2022-11-26 00:30:24 +0900

    Time: 524 ms (10.82%), Space: 56.5 MB (30.09%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.js
+++ 0575-distribute-candies/0575-distribute-candies.js
@@ -7,10 +7,12 @@ var distributeCandies = function(candyType) {
     const maximum = candyType.length / 2
     let data = {}
     let uniq = 0;
-    while (candyType.length && uniq < maximum) {
-        const candy = candyType.shift();
+    let i = 0;
+    while (i < candyType.length && uniq < maximum) {
+        const candy = candyType[i];
         if (!data[candy]) uniq ++;
         data[candy] = (data[candy] || 0) + 1;
+        i++;
     }
     return uniq;
 };
