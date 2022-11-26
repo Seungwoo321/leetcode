9e652fc744a2b666c7c829fe95230e29875dd2ed
Seungwoo321 <seungwoo321@gmail.com>
2022-11-26 23:27:01 +0900

    Time: 123 ms (91.87%), Space: 51.3 MB (73.68%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.js
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.js
@@ -3,21 +3,15 @@
  * @return {number}
  */
 var findLHS = function(nums) {
-    nums = nums.sort((a, b) => a - b);
-    const uniqueTable = new Set();
-    const countTable = new Map();
+    const hashTable = new Map();
     for (let i = 0; i < nums.length; i ++) {
-        const num = nums[i];
-        countTable.set(num, (countTable.get(num) || 0) + 1);
-        if (!uniqueTable.has(num)) {
-            uniqueTable.add(num);
-        }
+        hashTable.set(nums[i], (hashTable.get(nums[i]) || 0) + 1);
     }
     let max = 0;
-    uniqueTable.forEach(value => {
-        if (uniqueTable.has(value + 1)) {
-            max = Math.max(countTable.get(value) + countTable.get(value + 1) , max)  
+    hashTable.forEach((value, key) => {
+        if (hashTable.has(key + 1)) {
+            max = Math.max(value + hashTable.get(key + 1) , max);
         }
-    })
+    });
     return max;
 };
