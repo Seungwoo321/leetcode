de632901d559c27210ba1918f6512d8f3a69b083
Seungwoo321 <seungwoo321@gmail.com>
2022-11-26 21:43:45 +0900

    Time: 137 ms (45.33%), Space: 49.3 MB (13.87%) - LeetHub

--- 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.js
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.js
@@ -10,6 +10,9 @@
  * @param {Node|null} root
  * @return {number[]}
  */
-var postorder = function(root) {
-    return root ? [...root.children.map(postorder).flat(), root.val] : [];
+var postorder = function(root, visted = []) {
+    if (!root) return [];
+    root.children.forEach(node => postorder(node, visted))
+    if (root.val !== null) visted.push(root.val);
+    return visted;
 };
