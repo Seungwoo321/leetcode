10183c58c173a675d1309f6677f29a3f2ce92451
Seungwoo321 <seungwoo321@gmail.com>
2022-11-27 19:44:12 +0900

    Time: 111 ms (42.06%), Space: 44.5 MB (11.11%) - LeetHub

--- 0598-range-addition-ii/0598-range-addition-ii.js
+++ 0598-range-addition-ii/0598-range-addition-ii.js
@@ -6,7 +6,18 @@
  */
 var maxCount = function(m, n, ops) {
     if (!ops.length) return m * n;
-    const row = Math.min(...ops.map(m => m[0]));
-    const col = Math.min(...ops.map(m => m[1]));
-    return row * col;
+    // const row = Math.min(...ops.map(m => m[0]));
+    // const col = Math.min(...ops.map(m => m[1]));
+    // return row * col;
+    
+    return ops.reduce((acc, cur) => {
+        acc.row = Math.min(acc.row, cur[0]);
+        acc.col = Math.min(acc.col, cur[1]);
+        acc.answer = acc.row * acc.col;
+        return acc;
+    }, {
+        row: Infinity,
+        col: Infinity,
+        answer: 0
+    }).answer
 };
