7964c9523c94dde3137195796cc1af32bd5658bc
Seungwoo321 <seungwoo321@gmail.com>
2022-11-28 21:04:43 +0900

    Time: 155 ms (67.36%), Space: 50.6 MB (14.25%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.js
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.js
@@ -13,10 +13,8 @@
  */
 var mergeTrees = function(root1, root2) {
     if (!root1 || !root2) return root2 || root1;
-    if (root1 && root2) {
-        root1.val += root2.val;
-        root1.left = mergeTrees(root1.left, root2.left);
-        root1.right = mergeTrees(root1.right, root2.right);
-    }
+    root1.val += root2.val;
+    root1.left = mergeTrees(root1.left, root2.left);
+    root1.right = mergeTrees(root1.right, root2.right);
     return root1;
 };
