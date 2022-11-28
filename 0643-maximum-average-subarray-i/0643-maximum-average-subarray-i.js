9cb00f717639fe2cf287dc69ad7ef717d1555361
Seungwoo321 <seungwoo321@gmail.com>
2022-11-28 22:21:28 +0900

    Time: 195 ms (17.16%), Space: 59 MB (7.48%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function(nums, k) {
+    let arr = [0];
+    nums.forEach((num, i) => {
+        if (i < k) {
+            arr[0] += num;
+        } else {
+            arr.push(arr[arr.length - 1] - nums[i - k] + num);
+        }
+    });
+    return Math.max(...arr) / k;
+};
