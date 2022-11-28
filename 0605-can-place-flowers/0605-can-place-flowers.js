1a1f02e24edfbe586713dba6e10696b2d3be4215
Seungwoo321 <seungwoo321@gmail.com>
2022-11-28 18:50:04 +0900

    Time: 98 ms (70.94%), Space: 44.2 MB (63.30%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -7,15 +7,18 @@ var canPlaceFlowers = function(flowerbed, n) {
     if (n === 0) return true;
     let count = 0;
     for (let i = 0; i < flowerbed.length; i ++) {
-        let leftEmpty = i === 0 || flowerbed[i - 1] === 0;
-        let rightEmpty = i === flowerbed.length - 1 || flowerbed[i + 1] === 0;
-        if (leftEmpty && rightEmpty && flowerbed[i] === 0) {
-            count ++;
-            flowerbed[i] = 1;
-            if (count === n) {
-                return true;
+        if (flowerbed[i] === 0) {
+            let leftEmpty = i === 0 || flowerbed[i - 1] === 0;
+            let rightEmpty = i === flowerbed.length - 1 || flowerbed[i + 1] === 0;
+            if (leftEmpty && rightEmpty) {
+                count ++;
+                flowerbed[i] = 1;
+                if (count >= n) {
+                    return true;
+                }
             }
         }
+
     }
-    return false;
+    return count >= n;
 };
