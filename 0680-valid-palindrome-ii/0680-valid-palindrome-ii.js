d529b891746881833f537f36a8edbc85f139c662
Seungwoo321 <seungwoo321@gmail.com>
2022-11-30 00:30:59 +0900

    Time: 655 ms (5.09%), Space: 64.9 MB (9.59%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
@@ -2,9 +2,6 @@
  * @param {string} s
  * @return {boolean}
  */
-var reverseString = function (str) {
-  return (str === '') ? '' : reverseString(str.substr(1)) + str.charAt(0);
-}
 var validPalindrome = function(s) {
     let left = 0;
     let right = s.length - 1;
@@ -12,7 +9,7 @@ var validPalindrome = function(s) {
         if (s[left] !== s[right]) {
             let s1 = s.substr(0, left) + s.substr(left + 1);
             let s2 = s.substr(0, right) + s.substr(right + 1);
-            return s1 === reverseString(s1) || s2 === reverseString(s2)
+            return s1 === s1.split('').reverse().join('') || s2 === s2.split('').reverse().join('')
         }
         left ++;
         right --;
