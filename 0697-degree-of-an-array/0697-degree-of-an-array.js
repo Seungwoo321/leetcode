0675bee6358f5fd9ad609416e0c4e0fd3770d268
Seungwoo321 <seungwoo321@gmail.com>
2022-12-01 20:23:49 +0900

    Time: 77 ms (97.06%), Space: 46.1 MB (62.87%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.js
+++ 0697-degree-of-an-array/0697-degree-of-an-array.js
@@ -3,24 +3,28 @@
  * @return {number}
  */
 var findShortestSubArray = function(nums) {
-    const hashTable = new Map();
-    let degree = 0;
-    let minLen = 1;
+    const count = {};
+    const left = new Map();
+    const right = new Map();
+
     for (let i = 0; i < nums.length; i ++) {
         let n = nums[i];
-        if (hashTable.has(n)) {
-            const data = hashTable.get(n);
-            let frequency = (data.frequency || 0) + 1;
-            let len = i - data.first + 1;
-            hashTable.set(n, { frequency, first: data.first, len });
-            if (frequency > degree) {
-                degree = frequency;
-                minLen = len
-            } else if (frequency === degree) {
-                minLen = Math.min(minLen, len);
-            }
+        if (count[n]) {
+            count[n] = count[n] + 1;
+            right.set(n, i);
         } else {
-            hashTable.set(n, { frequency: 1, first: i, len: 0 });
+            count[n] = 1;
+            left.set(n, i);
+            right.set(n, i);
+        }
+    }
+    
+    let degree = Math.max(...Object.values(count));
+    let minLen = nums.length;
+    for (let k in count) {
+        const freq = count[k];
+        if (freq === degree) {
+            minLen = Math.min(minLen, right.get(Number(k)) - left.get(Number(k)) + 1);
         }
     }
     return minLen;
