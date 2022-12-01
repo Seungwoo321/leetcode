6b820a08464a39900cadd3004d220a7de0dbe00d
Seungwoo321 <seungwoo321@gmail.com>
2022-12-01 19:43:50 +0900

    Time: 113 ms (81.62%), Space: 50.5 MB (26.10%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.js
+++ 0697-degree-of-an-array/0697-degree-of-an-array.js
@@ -5,23 +5,23 @@
 var findShortestSubArray = function(nums) {
     const hashTable = new Map();
     let degree = 0;
-    let answer = 1;
+    let minLen = 1;
     for (let i = 0; i < nums.length; i ++) {
         let n = nums[i];
         if (hashTable.has(n)) {
             const data = hashTable.get(n);
-            let count = (data.count || 0) + 1;
-            let len = i - data.start + 1;
-            hashTable.set(n, { count, start: data.start, len });
-            if (count > degree) {
-                degree = count;
-                answer = len
-            } else if (count === degree) {
-                answer = Math.min(answer, len);
+            let frequency = (data.frequency || 0) + 1;
+            let len = i - data.first + 1;
+            hashTable.set(n, { frequency, first: data.first, len });
+            if (frequency > degree) {
+                degree = frequency;
+                minLen = len
+            } else if (frequency === degree) {
+                minLen = Math.min(minLen, len);
             }
         } else {
-            hashTable.set(n, { count: 1, start: i, len: 0 });
+            hashTable.set(n, { frequency: 1, first: i, len: 0 });
         }
     }
-    return answer;
+    return minLen;
 };
