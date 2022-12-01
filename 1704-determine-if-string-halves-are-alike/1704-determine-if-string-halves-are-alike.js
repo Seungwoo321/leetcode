fb0713e2d29f7467a5de1a181c867f4e7a682661
Seungwoo321 <seungwoo321@gmail.com>
2022-12-02 01:53:23 +0900

    Time: 57 ms (99.61%), Space: 42.2 MB (93.75%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.js
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.js
@@ -2,12 +2,6 @@
  * @param {string} s
  * @return {boolean}
  */
-var halvesAreAlike = function(s) {
-    let re = /[^aeiouAEIOU]/g;
-    let len = s.length / 2;
-    let left = s.substr(0, len);
-    let right = s.substr(len);
-    return left.replaceAll(re).length === right.replaceAll(re).length;
-    
-    // return s.substr(0, s.length / 2).replaceAll(/[^aeiouAEIOU]/g, '').length === s.substr(s.length / 2, s.length / 2).replaceAll(/[^aeiouAEIOU]/g, '').length;
+var halvesAreAlike = function(s) {    
+    return s.substr(0, s.length / 2).replaceAll(/[^aeiouAEIOU]/g, '').length === s.substr(s.length / 2).replaceAll(/[^aeiouAEIOU]/g, '').length;
 };
