5dba16e68435bfa3e1d860a4cf23aa51dc67bb00
Seungwoo321 <seungwoo321@gmail.com>
2022-12-02 19:39:22 +0900

    Time: 118 ms (61.23%), Space: 45 MB (13.76%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} color
+ * @return {number[][]}
+ */
+var floodFill = function(image, sr, sc, color, curr = image[sr][sc]) {
+    if ((sr < 0 || sr > image.length - 1) ||
+        (sc < 0 || sr > image[0].length - 1) ||
+        (image[sr][sc] !== curr || image[sr][sc] === color)
+    ) {
+        return image;
+    }
+    image[sr][sc] = color;
+    floodFill(image, sr - 1, sc, color, curr);
+    floodFill(image, sr + 1, sc, color, curr);
+    floodFill(image, sr, sc - 1, color, curr);
+    floodFill(image, sr, sc + 1, color, curr);
+    return image;
+};
