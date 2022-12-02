789009afc7332726370b1244fd1d6e42693627e4
Seungwoo321 <seungwoo321@gmail.com>
2022-12-02 20:53:54 +0900

    Time: 152 ms (37.77%), Space: 45.7 MB (46.31%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -3,30 +3,18 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    const visted = {};
     const dfs = (grid, row, col) => {
-        if (!visted[row]) visted[row] = {}
-        if (visted[row][col]) return visted[row][col];
-        if (!(row in grid) || !(col in grid[0])) {
-            return true;
+        if (!(row in grid) || !(col in grid[0]) || grid[row][col] === '0') {
+            return 1;
         }
-        visted[row][col] = true;
-        if (grid[row][col] === '1') {
-            return dfs(grid, row + 1, col) &&
-                dfs(grid, row - 1, col) &&
-                dfs(grid, row, col - 1) &&
-                dfs(grid, row, col + 1);
-        } else {
-            return visted[row][col];
-        }
+        grid[row][col] = '0';
+        return dfs(grid, row + 1, col) && dfs(grid, row - 1, col) && dfs(grid, row, col - 1) && dfs(grid, row, col + 1);
     }
     let lands = 0;
     for (let i = 0; i < grid.length; i ++) {
         for (let j = 0; j < grid[i].length;j ++) {
-            if (!visted[i]) visted[i] = {};
-            if (!visted[i][j] && grid[i][j] === '1') {
-                dfs(grid, i, j);
-                lands++;
+            if (grid[i][j] === '1') {
+                lands += dfs(grid, i, j);
             }
         }
     }
