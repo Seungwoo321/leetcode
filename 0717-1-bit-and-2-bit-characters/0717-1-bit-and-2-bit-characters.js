9c730e7024decd48fffbe3d6097fccae778accdc
Seungwoo321 <seungwoo321@gmail.com>
2022-12-03 21:47:24 +0900

    Time: 64 ms (92.45%), Space: 42 MB (77.36%) - LeetHub

--- 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.js
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.js
@@ -5,11 +5,8 @@
 var isOneBitCharacter = function(bits) {
     let p = 0;
     while (p < bits.length - 1) {
-        if (bits[p] === 1) {
-            p += 2;
-        } else {
-            p += 1;
-        }
+        if (bits[p] === 1) p ++;
+        p ++;
     }
     return bits.length -1 === p;
 };
