8cff55eaf76300bc66c0ed64f69da56beba55359
Seungwoo321 <seungwoo321@gmail.com>
2022-12-04 19:24:05 +0900

    Time: 83 ms (78.02%), Space: 42.1 MB (96.20%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
@@ -3,10 +3,12 @@
  * @return {number}
  */
 var minCostClimbingStairs = function (cost) {
-    const dp = [cost[0], cost[1]];
-    for (let i = 2; i < cost.length; i ++) {
-        dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+    let first = cost[0];
+    let second = cost[1];
+    for (let i = 2; i < cost.length; i += 2) {
+        first = cost[i] + Math.min(first, second);
+        second = (cost[i + 1] || 0) + Math.min(second, first);
     }
-    return Math.min(dp[dp.length - 1], dp[dp.length - 2]);
+    return Math.min(first, second);
 };
 
