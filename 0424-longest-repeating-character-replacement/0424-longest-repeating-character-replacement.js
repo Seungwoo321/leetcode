c42ea4d4f8d2ce7aebbd37b743d90d2e23e6a47a
Seungwoo321 <seungwoo321@gmail.com>
2022-12-05 20:18:41 +0900

    Time: 137 ms (53.70%), Space: 41.9 MB (96.15%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -5,13 +5,13 @@
  */
 var characterReplacement = function(s, k) {
     const count = {};
-    let maxCount = 0;
+    let maxFrequency = 0;
     let start = 0;
     let result = 0;
     for (let end = 0; end < s.length; end ++) {
         count[s[end]] = (count[s[end]] || 0) + 1;
-        maxCount = Math.max(count[s[end]], maxCount);
-        while (end - start + 1 - maxCount > k) {
+        maxFrequency = Math.max(count[s[end]], maxFrequency);
+        while (end - start + 1 - maxFrequency > k) {
             count[s[start]] --;
             start ++;
         }
