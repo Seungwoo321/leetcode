7f2edf3c63c318e0562968d378d5b58da8cd3c37
Seungwoo321 <seungwoo321@gmail.com>
2022-12-06 23:15:53 +0900

    Time: 108 ms (71.00%), Space: 43.7 MB (85.67%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.js
+++ 0299-bulls-and-cows/0299-bulls-and-cows.js
@@ -5,25 +5,17 @@
  */
 var getHint = function(secret, guess) {
     
-    const hashTable = new Map();
-    let a = 0;
-    let b = 0;
+    const hashTable = new Array(10).fill(0)
+    let bulls = 0;
+    let cows = 0;
     
-    for (let i = 0; i < secret.length; i ++) {
-        if (secret[i] !== guess[i]) {
-            hashTable.set(secret[i], (hashTable.get(secret[i]) || 0) + 1);
-        }
-    }
-    for (let i = 0; i < guess.length; i++) {
-        let count = hashTable.get(guess[i]);
+    for (let i = 0; i < secret.length; i++) {
         if (secret[i] === guess[i]) {
-            a ++;
-        } else if (count === 0) {
-            hashTable.delete(guess[i]);
-        } else if (count > 0) {
-            hashTable.set(guess[i], count - 1);
-            b ++;
+            bulls ++;
+        } else {
+            if (hashTable[secret[i]]++ < 0) cows++;
+            if (hashTable[guess[i]]-- > 0) cows++;
         }
     }
-    return a + 'A' + b + 'B';
+    return bulls + 'A' + cows + 'B';
 };
