19d22e94ca4079d4f9b1811848ecd3e40cd0c0a8
Seungwoo321 <seungwoo321@gmail.com>
2022-12-06 22:02:01 +0900

    Time: 97 ms (83.11%), Space: 42.9 MB (39.50%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -6,12 +6,12 @@
 var twoSum = function (nums, target) {
     const hashTable = new Map();
     for (let i = 0; i < nums.length; i ++) {
-        hashTable.set(nums[i], i);
-    }
-    for (let i = 0; i < nums.length; i ++) {
-        const find = target - nums[i]
-        if (hashTable.has(find) && hashTable.get(find) !== i) {
-            return [i, hashTable.get(find)];
+        const find = target - nums[i];
+        if (hashTable.has(find)) {
+            return [hashTable.get(find), i];
+        } else {
+            hashTable.set(nums[i], i);
         }
+        
     }
 };
