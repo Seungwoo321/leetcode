cd55ea296b9363a18d69ef9b27a6119fb5b618f7
Seungwoo321 <seungwoo321@gmail.com>
2022-12-07 21:07:53 +0900

    Time: 73 ms (77.95%), Space: 41.5 MB (92.24%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var decodeString = function(s) {
+    const stack = [];
+    let currNum = 0;
+    let currStr = '';
+    
+    for (let i = 0; i < s.length; i ++) {
+        let str = s[i];
+        if (str == '[') {
+            stack.push(currStr);
+            stack.push(currNum);
+            currNum = 0;
+            currStr = '';
+        } else if (str == ']') {
+            let prevNum = stack.pop();
+            let prevStr = stack.pop();
+            currStr = prevStr + currStr.repeat(prevNum);
+        } else if (Number(str) >= 0 && Number(str) <= 9) {
+            currNum = currNum * 10 + Number(str);
+        } else {
+            currStr += str;
+        }
+    }
+
+    return currStr;
+};
