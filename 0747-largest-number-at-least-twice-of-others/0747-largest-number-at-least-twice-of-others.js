ee4137464dba160b854bed053dfbc5cedef137c3
Seungwoo321 <seungwoo321@gmail.com>
2022-12-07 21:30:38 +0900

    Time: 56 ms (97.80%), Space: 43.3 MB (10.00%) - LeetHub

--- 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.js
+++ 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.js
@@ -3,6 +3,6 @@
  * @return {number}
  */
 var dominantIndex = function(nums) {
-    nums = nums.map((v, i) => [v, i]).sort((a, b) => b[0] - a[0]).slice(0, 2);
+    nums = nums.map((v, i) => [v, i]).sort((a, b) => b[0] - a[0]);
     return nums[0][0] >= nums[1][0] * 2 ? nums[0][1] : -1;
 };
