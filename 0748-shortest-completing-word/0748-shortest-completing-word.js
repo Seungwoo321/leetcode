c62808cd073b2d5e515e2308c879fbace7a93889
Seungwoo321 <seungwoo321@gmail.com>
2022-12-07 22:51:57 +0900

    Time: 133 ms (50.00%), Space: 44.3 MB (89.83%) - LeetHub

--- 0748-shortest-completing-word/0748-shortest-completing-word.js
+++ 0748-shortest-completing-word/0748-shortest-completing-word.js
@@ -7,12 +7,8 @@ var shortestCompletingWord = function(licensePlate, words) {
     words.sort((a, b) => a.length - b.length);
     const shortText = licensePlate.replaceAll(/[^A-Za-z]/g, '').toLowerCase();    
     const findRest = (word) => {
-        for (let i = 0; i < shortText.length; i ++) {
-            word = word.replace(shortText[i], '');
-        }
+        [...shortText].forEach(s => word = word.replace(s, ''))
         return word.length;
     }
-    return words.find(word => {
-        return word.length >= shortText.length && word.length - shortText.length === findRest(word);
-    });
+    return words.find(word => word.length >= shortText.length && word.length - shortText.length === findRest(word));
 };
