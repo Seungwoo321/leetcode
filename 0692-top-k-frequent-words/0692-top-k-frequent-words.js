1388c595052ebcd5b7041805442480df3c63cb8f
Seungwoo321 <seungwoo321@gmail.com>
2022-12-08 23:39:52 +0900

    Time: 104 ms (78.56%), Space: 47.4 MB (32.70%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.js
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.js
@@ -4,14 +4,13 @@
  * @return {string[]}
  */
 var topKFrequent = function(words, k) {
-    words.sort();
-    
+
     return Object.values(words.reduce((acc, cur) => {
         if (!acc[cur]) acc[cur] = [];
         acc[cur] = [cur, (acc[cur][1] || 0) + 1];
         return acc;
     }, {}))
-        .sort((a, b) => b[1] - a[1])
+        .sort((a, b) => b[1] === a[1] ? a[0].localeCompare(b[0]) : b[1] - a[1])
         .slice(0, k)
         .map(v => v[0])
 };
