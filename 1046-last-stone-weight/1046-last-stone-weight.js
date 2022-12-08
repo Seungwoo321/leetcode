127ea271eecb5c1499315f89b8d02a232559296b
Seungwoo321 <seungwoo321@gmail.com>
2022-12-08 22:22:36 +0900

    Time: 89 ms (73.44%), Space: 43.5 MB (55.13%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.js
+++ 1046-last-stone-weight/1046-last-stone-weight.js
@@ -3,10 +3,22 @@
  * @return {number}
  */
 var lastStoneWeight = function(stones) {
-    while (stones.length > 1) {
+    while (stones.length > 2) {
         stones.sort((a, b) => b - a);
         stones[1] = stones[0] - stones[1];
         stones.shift();
     }
-    return stones[0];
+    return Math.abs(stones[0] - (stones[1] || 0));
+    
+//     stones.sort((a, b) => b - a);
+//     while (stones.length > 2) {
+//         const firstStone = stones.shift();
+//         const secondStone = stones.shift();
+        
+//         if (firstStone !== secondStone) {
+//             stones.push(firstStone - secondStone);
+//             stones.sort((a, b) => b - a);
+//         }
+//     }
+//     return Math.abs(stones[0] - (stones[1] || 0));
 }
