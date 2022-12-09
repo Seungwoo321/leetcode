dafa319a47be9bc05bb2146308143548c7e6b5e4
Seungwoo321 <seungwoo321@gmail.com>
2022-12-09 21:27:07 +0900

    Time: 98 ms (71.05%), Space: 44.1 MB (55.80%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} matrix
+ * @return {boolean}
+ */
+var isToeplitzMatrix = function(matrix) {
+    const m = matrix.length;
+    const n = matrix[0].length;
+
+    for (let i = 1; i < m; i ++) {
+        for (let j = 1; j < n; j ++) {
+            if (matrix[i][j] === matrix[i - 1][j - 1]) {
+                // arr[j - 1] = matrix[i][j - 1];
+            } else {
+                return false;
+            }
+           
+        }
+    }
+
+    return true;
+};
