45b5d350576296fc5d429c7955c7e4b0b92e4bb5
Seungwoo321 <seungwoo321@gmail.com>
2022-12-09 20:02:12 +0900

    Time: 123 ms (92.11%), Space: 42.4 MB (90.35%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.js
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.js
@@ -5,21 +5,18 @@
  */
 var countPrimeSetBits = function(left, right) {
     let count = 0;
-    const countBits = (n) => {
-        const dp = [0];
-        let offset = 1;
-        for (let i = 1; i <= n; i ++) {
-            if (offset * 2 === i) {
-                offset = i;
-            }
-            dp[i] = dp[i - offset] + 1;
+    const getNumOfSetBits = function(n) {
+        let result = 0;
+        while (n) {
+            result += (n & 1);
+            n = n >> 1;
         }
-        return dp;
-    };
-    const dp = countBits(right)
+        return result;
+    }
+
     const primes= [2, 3, 5, 7, 11, 13, 17, 19]
     while (left <= right) {
-        if (primes.includes(dp[left])) {
+        if (primes.includes(getNumOfSetBits(left))) {
             count ++;
         }
         left ++;
