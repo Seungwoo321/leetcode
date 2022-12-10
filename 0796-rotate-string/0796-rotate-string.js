6abdf2fd11af03d703681eaac062e502033faafb
Seungwoo321 <seungwoo321@gmail.com>
2022-12-10 22:17:43 +0900

    Time: 120 ms (8.24%), Space: 41.9 MB (67.58%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.js
+++ 0796-rotate-string/0796-rotate-string.js
@@ -6,13 +6,19 @@
 var rotateString = function(s, goal) {
     if (s === goal) return true;
     if (s.length !== goal.length) return false;
-    let ss = s + s;
-    for (let i = 0; i < ss.length; i ++) {
-        if (ss[i] === goal[0] && ss[i + goal.length - 1] === goal[goal.length - 1] ) {
-            if (ss.substr(i, goal.length) === goal) {
+    s = [...s];
+    let i = 0;
+    while (i < s.length) {
+        s.push(s.shift());
+        if (
+            s[0] === goal[0] && 
+            s[s.length - 1] === goal[goal.length - 1]
+        ) {
+            if (s.join('') === goal) {
                 return true;
             }
         }
+        i++;
     }
     return false;
 };
