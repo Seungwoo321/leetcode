8828f5dc933d7a8e088a58d9672a727b172d8f58
Seungwoo321 <seungwoo321@gmail.com>
2022-12-10 22:12:41 +0900

    Time: 103 ms (29.49%), Space: 41.8 MB (76.74%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.js
+++ 0796-rotate-string/0796-rotate-string.js
@@ -4,5 +4,15 @@
  * @return {boolean}
  */
 var rotateString = function(s, goal) {
-    return goal.length === s.length && (s + s).includes(goal);
+    if (s === goal) return true;
+    if (s.length !== goal.length) return false;
+    let ss = s + s;
+    for (let i = 0; i < ss.length; i ++) {
+        if (ss[i] === goal[0] && ss[i + goal.length - 1] === goal[goal.length - 1] ) {
+            if (ss.substr(i, goal.length) === goal) {
+                return true;
+            }
+        }
+    }
+    return false;
 };
