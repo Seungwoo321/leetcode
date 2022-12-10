a59609a0819b82bfcd564e91b95cc1a680bdda22
Seungwoo321 <seungwoo321@gmail.com>
2022-12-10 23:09:46 +0900

    Time: 95 ms (67.44%), Space: 44.2 MB (29.29%) - LeetHub

--- 0804-unique-morse-code-words/0804-unique-morse-code-words.js
+++ 0804-unique-morse-code-words/0804-unique-morse-code-words.js
@@ -5,5 +5,4 @@
 var uniqueMorseRepresentations = function(words) {
     const morseCodes = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
     return new Set(words.map(word => [...word].map(c => morseCodes[(c.charCodeAt() - 97) % 26]).join(''))).size;
-    
 };
