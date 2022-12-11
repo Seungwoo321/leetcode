1f50be6782ff4abc07a600330f5c5efa1007931b
Seungwoo321 <seungwoo321@gmail.com>
2022-12-11 22:43:50 +0900

    Time: 78 ms (94.17%), Space: 44.8 MB (93.20%) - LeetHub

---
+++ 0830-positions-of-large-groups/0830-positions-of-large-groups.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number[][]}
+ */
+var largeGroupPositions = function(s) {
+    const groups = [];
+    let start = 0;
+    for (let i = 1; i < s.length + 1; i ++) {
+        if (s[start] !== s[i]) {
+            let end = i - 1;
+            if (end - start >= 2) {
+                groups.push([start, end]);
+            }
+            start = i;
+        }
+    }
+    return groups
+};
