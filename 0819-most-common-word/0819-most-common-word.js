d3616b5812600c7356e857ebe54e9121912b1390
Seungwoo321 <seungwoo321@gmail.com>
2022-12-11 20:20:54 +0900

    Time: 91 ms (77.13%), Space: 44.7 MB (43.05%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.js
+++ 0819-most-common-word/0819-most-common-word.js
@@ -6,14 +6,14 @@
 var mostCommonWord = function(paragraph, banned) {
     const bannedSet = new Set(banned);
     const freqMap = new Map();
-    let maxFreq = ''
-    let maxFreqStr = '';
+    let freqStr = ''
+    let freqCount = '';
     paragraph.replaceAll(/([!?',;.])/g, ' ').toLowerCase().split(' ').forEach(str => {
         freqMap.set(str, (freqMap.get(str) || 0) + 1);
-        if (str && !bannedSet.has(str) && freqMap.get(str) > maxFreq) {
-            maxFreq = freqMap.get(str);
-            maxFreqStr = str; 
+        if (str && !bannedSet.has(str) && freqMap.get(str) > freqCount) {
+            freqCount = freqMap.get(str);
+            freqStr = str; 
         }
     });
-    return maxFreqStr;
+    return freqStr;
 };
