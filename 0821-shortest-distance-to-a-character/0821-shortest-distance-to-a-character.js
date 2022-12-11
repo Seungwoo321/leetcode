1168242fede9c61916bcb2c01b1d019c55a57318
Seungwoo321 <seungwoo321@gmail.com>
2022-12-11 21:20:13 +0900

    Time: 125 ms (37.39%), Space: 44.2 MB (68.91%) - LeetHub

---
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @param {character} c
+ * @return {number[]}
+ */
+var shortestToChar = function(s, c) {
+    let first = s.indexOf(c);
+    let next = first;
+    const distance = [];
+    for (let i = 0; i < s.length; i ++) {
+        if (s[i] === c) {
+            first = i;
+            next = s.substr(first + 1).indexOf(c) + first + 1;
+        }
+        distance.push(Math.min(Math.abs(first - i), Math.abs(next - i)));
+    }
+    return distance;
+};
