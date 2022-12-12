89aa9a7196c87863ecbaf365336c6a76e786b2a2
Seungwoo321 <seungwoo321@gmail.com>
2022-12-12 21:01:34 +0900

    Time: 136 ms (58.94%), Space: 50.4 MB (89.40%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} bills
+ * @return {boolean}
+ */
+var lemonadeChange = function(bills) {
+    let five = 0;
+    let ten = 0;
+    
+    for (let i = 0; i < bills.length; i ++) {
+        if (bills[i] === 5) {
+            five ++;
+        } else if (bills[i] === 10) {
+            five --;
+            ten ++;
+        } else if (bills[i] === 20) {
+            if (ten > 0) {
+                ten --;
+                five --;
+            } else {
+                five -= 3;
+            }
+        }
+        if (five < 0) return false;
+    }
+    return true;
+};
