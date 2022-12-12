b74e99837266297b3b5e23edce5223a4b37f4f9f
Seungwoo321 <seungwoo321@gmail.com>
2022-12-12 20:09:52 +0900

    Time: 118 ms (37.14%), Space: 44.1 MB (31.43%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var buddyStrings = function(s, goal) {
+    if (s.length !== goal.length) return false;
+    
+    let diff = []
+    for (let i = 0; i < s.length; i ++) {
+        if (s[i] !== goal[i]) {
+           diff.push(i);
+        }
+    }
+    if (diff.length === 0) {
+        return new Set(s).size !== s.length;
+    }
+    if (diff.length === 2) {
+        return s[diff[0]] === goal[diff[1]] && s[diff[1]] === goal[diff[0]];
+    }
+    return false;
+};
