aa2eb78824651e55b10ca01ce8da9d37b80ee0fe
Seungwoo321 <seungwoo321@gmail.com>
2022-12-13 22:08:17 +0900

    Time: 102 ms (50.91%), Space: 42.9 MB (45.45%) - LeetHub

---
+++ 0883-projection-area-of-3d-shapes/0883-projection-area-of-3d-shapes.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var projectionArea = function(grid) {
+    
+
+    let answer = 0;
+    for (let i = 0; i < grid.length; i ++) {
+        let x = 0;
+        let y = 0;
+        for (let j = 0; j < grid.length; j ++) {
+            x = Math.max(x, grid[i][j]);
+            y = Math.max(y, grid[j][i]);
+            if (grid[i][j]) answer ++;
+        }
+        answer += x + y;
+    }
+    return answer;
+};
