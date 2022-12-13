2db8e7c6adcb67a8eb6381992a3116382ae10fd7
Seungwoo321 <seungwoo321@gmail.com>
2022-12-13 19:17:48 +0900

    Time: 92 ms (80.00%), Space: 45 MB (23.25%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.js
+++ 0867-transpose-matrix/0867-transpose-matrix.js
@@ -3,10 +3,12 @@
  * @return {number[][]}
  */
 var transpose = function(matrix) {
-    const newMatrix = new Array(matrix[0].length).fill(0).map(v => new Array(matrix.length).fill(0));
+    let m = matrix.length;
+    let n = matrix[0].length;
+    const newMatrix = new Array(n).fill(0).map(v => new Array(m).fill(0));
 
-    for (let i = 0; i < matrix.length; i ++) {
-        for (let j = 0; j < matrix[0].length; j ++) {
+    for (let i = 0; i < m; i ++) {
+        for (let j = 0; j < n; j ++) {
             newMatrix[j][i] = matrix[i][j];
         }
     }
