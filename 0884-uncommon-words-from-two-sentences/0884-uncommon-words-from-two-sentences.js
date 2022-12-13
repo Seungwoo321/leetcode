c61422534f205c1f315ae1c6d3076df0c4dc092e
Seungwoo321 <seungwoo321@gmail.com>
2022-12-13 22:45:18 +0900

    Time: 101 ms (38.46%), Space: 41.8 MB (98.29%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.js
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.js
@@ -4,16 +4,5 @@
  * @return {string[]}
  */
 var uncommonFromSentences = function(s1, s2) {
-    const count = new Map();
-    const ss = [...s1.split(' '), ...s2.split(' ')];
-    const uncommon = [];
-    for (let i = 0; i < ss.length; i ++) {
-        count.set(ss[i], (count.get(ss[i]) || 0) + 1);
-    }
-    for (let key of count) {
-        if (key[1] === 1) {
-            uncommon.push(key[0]);
-        }
-    }
-    return uncommon;
+    return [...s1.split(' '), ...s2.split(' ')].filter((s, i, arr) => arr.indexOf(s) === arr.lastIndexOf(s));
 };
