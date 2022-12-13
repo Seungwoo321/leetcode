1bfd6eede50b37f04d1dc22ecece28f716a5c5cc
Seungwoo321 <seungwoo321@gmail.com>
2022-12-13 20:22:59 +0900

    Time: 103 ms (30.97%), Space: 42.2 MB (30.09%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.js
+++ 0868-binary-gap/0868-binary-gap.js
@@ -3,14 +3,16 @@
  * @return {number}
  */
 var binaryGap = function(n) {
-    n = n.toString(2)
-    let start = n.indexOf('1');
-    const ditance = [];
-    for (let i = start; i < n.length; i ++) {
-        if (n[i] === '1') {
-            ditance.push(i - start);
-            start = i;
+    let last = -1;
+    let ditance = 0;
+    let i = 0;
+    while (n) {
+        if (n & 1) {
+            if (last > -1) ditance = Math.max(ditance, i - last);
+            last = i;
         }
+        n = n >> 1;
+        i ++;
     }
-    return Math.max(...ditance);
+    return ditance;
 };
