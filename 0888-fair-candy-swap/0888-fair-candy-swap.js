4686dcb9efe6fe2227a8d00a36787a968379d426
Seungwoo321 <seungwoo321@gmail.com>
2022-12-14 19:49:24 +0900

    Time: 146 ms (70.15%), Space: 48.8 MB (66.42%) - LeetHub

--- 0888-fair-candy-swap/0888-fair-candy-swap.js
+++ 0888-fair-candy-swap/0888-fair-candy-swap.js
@@ -6,13 +6,32 @@
 var fairCandySwap = function(aliceSizes, bobSizes) {
     const sum = (acc, cur) => acc + cur
     const findDiff = (aliceSizes.reduce(sum, 0) - bobSizes.reduce(sum, 0)) / 2;
-    
-    const aliceSet = new Set(aliceSizes);
-    
-    for (let size of bobSizes) {
-        if (aliceSet.has(findDiff + size)) {
-            return [findDiff + size, size];
-        }
-    }  
-};
+    aliceSizes.sort((a, b) => a - b);
+    bobSizes.sort((a, b) => a - b);
 
+
+    const binarySearch = (target, arr) => {
+        let left = 0;
+        let right = arr.length;
+        while (left < right) {
+            let mid = Math.floor((left + right) / 2);
+            if (arr[mid] > target) {
+                right = mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1
+    }
+    
+    for (let j = 0; j < bobSizes.length; j ++) {
+
+        let i = binarySearch(findDiff + bobSizes[j], aliceSizes);
+        if (i > -1) {
+            return [aliceSizes[i], bobSizes[j]]
+        }
+    
+    }
+};
