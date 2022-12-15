e77468dbbb1fb38618ec5abd0b6b8f6edd92770c
Seungwoo321 <seungwoo321@gmail.com>
2022-12-15 19:55:56 +0900

    Time: 83 ms (90.45%), Space: 45.2 MB (16.09%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.js
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.js
@@ -7,7 +7,10 @@ var sortArrayByParity = function(nums) {
     let j = nums.length - 1;
     while (i < j) {
         if (nums[i] % 2 > nums[j] % 2) {
-            [nums[i], nums[j]] = [nums[j], nums[i]];
+            // [nums[i], nums[j]] = [nums[j], nums[i]];
+            let tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
         }
         if (nums[i] % 2 === 0) i ++;
         if (nums[j] % 2 === 1) j --;
