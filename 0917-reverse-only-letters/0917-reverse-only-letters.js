52762d3c5284d3d6b6e19e4aeefb8c30da529653
Seungwoo321 <seungwoo321@gmail.com>
2022-12-15 22:08:45 +0900

    Time: 59 ms (97.42%), Space: 42.3 MB (45.02%) - LeetHub

--- 0917-reverse-only-letters/0917-reverse-only-letters.js
+++ 0917-reverse-only-letters/0917-reverse-only-letters.js
@@ -6,15 +6,15 @@ var reverseOnlyLetters = function(s) {
     let p1 = 0;
     let p2 = s.length - 1;
     s = [...s]
-    const reversibleChar = c => (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');  
+    const isAlpha = c => (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');  
     while (p1 < p2) {
-        if (reversibleChar(s[p1]) && reversibleChar(s[p2])) {
+        if (isAlpha(s[p1]) && isAlpha(s[p2])) {
             [s[p1], s[p2]] = [s[p2], s[p1]];
             p1 ++;
             p2 --;
         }
-        if (!reversibleChar(s[p1])) p1 ++;
-        if (!reversibleChar(s[p2])) p2 --;
+        if (!isAlpha(s[p1])) p1 ++;
+        if (!isAlpha(s[p2])) p2 --;
     }
     return s.join('');
 };
