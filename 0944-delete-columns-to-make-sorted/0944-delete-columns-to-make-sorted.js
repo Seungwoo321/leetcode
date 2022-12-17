333e7dd7778dd34d18f646cc9bda6d2fdea8112d
Seungwoo321 <seungwoo321@gmail.com>
2022-12-17 22:30:45 +0900

    Time: 111 ms (68.99%), Space: 46.1 MB (70.54%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.js
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.js
@@ -3,15 +3,12 @@
  * @return {number}
  */
 var minDeletionSize = function(strs) {
-    let count = 0
+    let count = 0;
     for (let i = 0; i < strs[0].length; i ++) {
-        let prev = strs[0][i];
         for (let j = 1; j < strs.length; j ++) {
-            if (prev > strs[j][i]) {
+            if (strs[j - 1][i] > strs[j][i]) {
                 count ++;
                 break;
-            } else {
-                prev = strs[j][i];
             }
         }
     }
