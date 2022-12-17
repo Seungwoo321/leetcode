e3fdc05846ee162d874cae74d37c9609a7fbc7f6
Seungwoo321 <seungwoo321@gmail.com>
2022-12-17 21:53:01 +0900

    Time: 100 ms (81.06%), Space: 45 MB (35.24%) - LeetHub

--- 0942-di-string-match/0942-di-string-match.js
+++ 0942-di-string-match/0942-di-string-match.js
@@ -5,7 +5,7 @@
 var diStringMatch = function(s) {
     const nums = [new Array(s.length).fill(0)];
     let I = 0;
-    let D = s.length
+    let D = s.length;
     for (let i = 0; i < s.length; i ++) {
         if (s[i] === 'I') {
             nums[i] = I++;
