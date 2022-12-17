700d4533be817d0f1585bc4ebbed7cc68bd239a6
Seungwoo321 <seungwoo321@gmail.com>
2022-12-17 20:02:21 +0900

    Time: 1036 ms (18.04%), Space: 56.4 MB (56.70%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.js
@@ -0,0 +1,24 @@
+
+var RecentCounter = function() {
+    this.queue = [];
+    this.range = (t) => {
+        const limit = t - 3000;
+        return () => [Math.min(limit, t), Math.max(limit, t)]
+    }
+};
+
+/** 
+ * @param {number} t
+ * @return {number}
+ */
+RecentCounter.prototype.ping = function(t) {
+    const range = this.range(t)();
+    this.queue.push(t);
+    return this.queue.filter(q => q >= range[0] && q <= range[1]).length;
+};
+
+/** 
+ * Your RecentCounter object will be instantiated and called as such:
+ * var obj = new RecentCounter()
+ * var param_1 = obj.ping(t)
+ */
