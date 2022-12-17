1e6e21d4cca8038193d1e54ecd71f6b43168568f
Seungwoo321 <seungwoo321@gmail.com>
2022-12-17 19:28:49 +0900

    Time: 81 ms (92.41%), Space: 46.4 MB (78.16%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.js
+++ 0929-unique-email-addresses/0929-unique-email-addresses.js
@@ -6,8 +6,7 @@ var numUniqueEmails = function(emails) {
     const emailSet = new Set();
     emails.forEach(email => {
         email = email.split('@');
-        const localName = email[0].replaceAll('.', '').split('+')[0];
-        emailSet.add([localName, email[1]].join('@'));
+        emailSet.add([email[0].replaceAll('.', '').split('+')[0], email[1]].join('@'));
     });
     return emailSet.size;
 };
