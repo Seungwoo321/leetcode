66d72815379e704a95eec6b7df66ea092e15e13c
Seungwoo321 <seungwoo321@gmail.com>
2022-12-17 21:00:03 +0900

    Time: 239 ms (77.45%), Space: 73 MB (97.41%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.js
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.js
@@ -13,6 +13,9 @@
  * @return {number}
  */
 var rangeSumBST = function(root, low, high) {
-    if (!root) return 0;    
-    return rangeSumBST(root.left, low, high) + (root.val >= low && root.val <= high ? root.val : 0) + rangeSumBST(root.right, low, high);
+    return (
+        (root?.left ? rangeSumBST(root.left, low, high) : 0) +
+        (root.val >= low  && root.val <= high ? root.val : 0) +
+        (root?.right ? rangeSumBST(root.right, low, high) : 0)
+    );
 };
