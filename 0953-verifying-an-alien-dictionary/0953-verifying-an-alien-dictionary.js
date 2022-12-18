9e1721697361f4dd394dde8f99ba27ec6b631c5a
Seungwoo321 <seungwoo321@gmail.com>
2022-12-18 20:46:56 +0900

    Time: 90 ms (60.86%), Space: 42.7 MB (56.03%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.js
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.js
@@ -4,21 +4,18 @@
  * @return {boolean}
  */
 var isAlienSorted = function(words, order) {
-    const orderMap = [...order].reduce((acc, cur, i) => {
-        acc[cur] = i + 1;
-        return acc;
-    }, {});
-
-    for (let i = 1; i < words.length; i ++) {
-        const maxLength = Math.max(words[i - 1].length, words[i].length);
-        for (let j = 0; j < maxLength; j ++) {
-            if (words[i - 1][j] === words[i][j]) continue;
-            if ((orderMap[words[i - 1][j]] || 0) < (orderMap[words[i][j]] || 0)) {
-                break;
-            } else {
-                return false;
-            }
-            
+    let mapping = new Map([[undefined, -1]]);
+    for (let i = 0; i < order.length; i++) {
+        mapping.set(order[i], i);
+    }
+    for (let i = 1; i < words.length; i++) {
+        let a = words[i - 1];
+        let b = words[i];
+        for (let j = 0; j < Math.max(a.length, b.length); j++) {
+            const aIndex = mapping.get(a[j]);
+            const bIndex = mapping.get(b[j]);
+            if (aIndex < bIndex) break;
+            if (aIndex > bIndex) return false;
         }
     }
     return true;
