4d6d79a87b0f9aae45e026109e0259276ace7014
Seungwoo321 <seungwoo321@gmail.com>
2022-12-18 21:34:39 +0900

    Time: 57 ms (98.58%), Space: 42.6 MB (49.29%) - LeetHub

--- 0965-univalued-binary-tree/0965-univalued-binary-tree.js
+++ 0965-univalued-binary-tree/0965-univalued-binary-tree.js
@@ -11,8 +11,7 @@
  * @return {boolean}
  */
 var isUnivalTree = function(root, value = root.val) {
-    if (root === null) return true;
-    return root.val === value &&
+    return root === null || (root.val === value &&
         isUnivalTree(root.left, root.val) &&
-        isUnivalTree(root.right, root.val);
+        isUnivalTree(root.right, root.val));
 };
