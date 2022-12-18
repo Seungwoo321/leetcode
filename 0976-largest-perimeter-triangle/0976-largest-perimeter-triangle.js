909a72a57cb890e62f8a19074c913e913314d8c9
Seungwoo321 <seungwoo321@gmail.com>
2022-12-18 22:05:11 +0900

    Time: 191 ms (24.14%), Space: 46.1 MB (19.82%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.js
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.js
@@ -6,11 +6,8 @@ var largestPerimeter = function(nums) {
     nums.sort((a, b) => b - a);
     let perimeter = 0;
     for (let i = 2; i < nums.length; i ++) {
-        const a = nums[i - 2];
-        const b = nums[i - 1];
-        const c = nums[i];
-        if (a + b > c && a + c > b && b + c > a) {
-            perimeter = Math.max(a + b + c, perimeter);
+        if (nums[i] + nums[i - 1] > nums[i - 2]) {
+            perimeter = Math.max(nums[i] + nums[i - 1] + nums[i - 2], perimeter);
         }
     }
     return perimeter;
