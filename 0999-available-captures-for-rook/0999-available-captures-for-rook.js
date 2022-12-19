df154ece4f1bb973fe4398962851b05e09e62246
Seungwoo321 <seungwoo321@gmail.com>
2022-12-19 21:57:35 +0900

    Time: 87 ms (47.75%), Space: 41.8 MB (63.96%) - LeetHub

--- 0999-available-captures-for-rook/0999-available-captures-for-rook.js
+++ 0999-available-captures-for-rook/0999-available-captures-for-rook.js
@@ -3,35 +3,31 @@
  * @return {number}
  */
 var numRookCaptures = function(board) {
-    
-    let capture = 0;
-    const rowCapture = new Array(8).fill(0);
-    const columnCapture = new Array(8).fill(0);
-    for (let i = 0; i < 8; i ++) {
-        for (let j = 0; j < 8; j ++) {
-            if (board[i][j] === 'R') {
-                capture = rowCapture[i] + columnCapture[j];
-                let [x, y] = [i, j];
-                while (i < 8) {
-                    if (board[i][y] === 'p' || board[i][y] === 'B') {
-                        capture += +(board[i][y] === 'p')
-                        break;
-                    }
-                    i ++;
-                }
-                while (j < 8) {
-                    if (board[x][j] === 'p' || board[x][j] === 'B') {
-                        capture += +(board[x][j] === 'p')
-                        break;
-                    }
-                    j ++;
-                }
-                return capture;
-            }
-            if (board[i][j] === 'p' || board[i][j] === 'B') {
-                rowCapture[i] = +(board[i][j] === 'p');
-                columnCapture[j] = +(board[i][j] === 'p');
-            }
+    let [x, y] = [0, 0];
+    for (let row in board) {
+        if (board[row].indexOf('R') > -1) {
+            [x, y] = [row, board[row].indexOf('R')];
+            break;
         }
     }
+    let captures = [0, 0];  
+    let i = 0;
+    let j = 0;
+    while (i < 8) {
+        if (board[i][y] === 'p' || board[i][y] === 'B') {
+            let n = +(board[i][y] === 'p');
+            captures[0] = (i < x ? 0 : captures[0]) + n;
+            if (i > x) break;
+        }
+        i ++;
+    }
+    while (j < 8) {
+        if (board[x][j] === 'p' || board[x][j] === 'B') {
+            let n = +(board[x][j] === 'p');
+            captures[1] = (j < y ? 0 : captures[1]) + n;
+            if (j > y) break;
+        }
+        j ++;
+    }
+    return captures[0] + captures[1];
 };
