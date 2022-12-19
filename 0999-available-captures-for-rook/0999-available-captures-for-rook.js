47e5682ad25f943a596cb070f457352e21cd5007
Seungwoo321 <seungwoo321@gmail.com>
2022-12-19 21:41:44 +0900

    Time: 65 ms (81.98%), Space: 41.5 MB (90.09%) - LeetHub

--- 0999-available-captures-for-rook/0999-available-captures-for-rook.js
+++ 0999-available-captures-for-rook/0999-available-captures-for-rook.js
@@ -3,41 +3,35 @@
  * @return {number}
  */
 var numRookCaptures = function(board) {
-    let [x, y] = [0, 0];
-    for (let row in board) {
-        if (board[row].indexOf('R') > -1) {
-            [x, y] = [row, board[row].indexOf('R')];
-            break;
+    
+    let capture = 0;
+    const rowCapture = new Array(8).fill(0);
+    const columnCapture = new Array(8).fill(0);
+    for (let i = 0; i < 8; i ++) {
+        for (let j = 0; j < 8; j ++) {
+            if (board[i][j] === 'R') {
+                capture = rowCapture[i] + columnCapture[j];
+                let [x, y] = [i, j];
+                while (i < 8) {
+                    if (board[i][y] === 'p' || board[i][y] === 'B') {
+                        capture += +(board[i][y] === 'p')
+                        break;
+                    }
+                    i ++;
+                }
+                while (j < 8) {
+                    if (board[x][j] === 'p' || board[x][j] === 'B') {
+                        capture += +(board[x][j] === 'p')
+                        break;
+                    }
+                    j ++;
+                }
+                return capture;
+            }
+            if (board[i][j] === 'p' || board[i][j] === 'B') {
+                rowCapture[i] = +(board[i][j] === 'p');
+                columnCapture[j] = +(board[i][j] === 'p');
+            }
         }
     }
-    let captures = [0, 0];  
-    let i = 0;
-    let j = 0;
-    while (i < x) {
-        if (board[i][y] === 'p' || board[i][y] === 'B') {
-            captures[0] = +(board[i][y] === 'p');
-        }
-        i ++;
-    }
-    while (i < 8) {
-        if (board[i][y] === 'p' || board[i][y] === 'B') {
-            captures[0] += +(board[i][y] === 'p');
-            break;
-        }
-        i ++;
-    }
-    while (j < y) {
-        if (board[x][j] === 'p' || board[x][j] === 'B') {
-            captures[1] = +(board[x][j] === 'p');
-        }
-        j ++;
-    }
-    while (j < 8) {
-        if (board[x][j] === 'p' || board[x][j] === 'B') {
-            captures[1] += +(board[x][j] === 'p');
-            break;
-        }
-        j ++;
-    }
-    return captures[0] + captures[1];
 };
