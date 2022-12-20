ea5d776c978b665251e8fa2dc79403955262ad29
Seungwoo321 <seungwoo321@gmail.com>
2022-12-20 21:32:05 +0900

    Time: 101 ms (29.27%), Space: 42.3 MB (9.76%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.js
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.js
@@ -3,8 +3,5 @@
  * @return {number}
  */
 var bitwiseComplement = function(n) {
-    if (n === 0) return 1;
-    let bit = Math.log2(n) + 1;
-    let mask = (1 << bit) - 1;
-    return n ^ mask;
+    return (2 << (Math.log2(Math.max(n, 1)))) - 1 - n;
 };
