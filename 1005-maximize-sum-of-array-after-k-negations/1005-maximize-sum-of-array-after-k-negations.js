4556a08b1a3f89d4086e65afa5f53e14e8b10b12
Seungwoo321 <seungwoo321@gmail.com>
2022-12-20 20:48:18 +0900

    Time: 95 ms (75.23%), Space: 44.2 MB (52.29%) - LeetHub

--- 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.js
+++ 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.js
@@ -3,12 +3,15 @@
  * @param {number} k
  * @return {number}
  */
-var largestSumAfterKNegations = function(nums, k) {
+var largestSumAfterKNegations = function(nums, k) {    
+    nums.sort((a, b) => a - b);
+    let modify = 0;
     let i = 0;
-    while (i < k) {
-        nums.sort((a, b) => a - b);
-        nums[0] = -nums[0];
-        i ++;
+    while (k > 0) {
+        if (nums[i] < nums[modify]) modify = i;
+        if (i + 1 < nums.length) i ++;
+        nums[modify] = -nums[modify];
+        k --;
     }
     return nums.reduce((acc, cur) => acc + cur, 0);
 };
