49b1dcbc620f98c2503410d2ca71981b3e248c5a
Seungwoo321 <seungwoo321@gmail.com>
2022-12-21 21:33:57 +0900

    Time: 75 ms (86.64%), Space: 43.8 MB (68.95%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.js
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.js
@@ -5,19 +5,12 @@
 var removeOuterParentheses = function(s) {
     let answer = '';
     let opend = 0;
-    const stack = [];
     [...s].forEach(p => {
-        if (p === '(') {
-            if (opend > 0) {
-                answer += p;
-            }
-            opend ++;
+        if (p === '(' && opend++ > 0) {
+            answer += p;
         }
-        if (p === ')') {
-            opend --; 
-            if (opend > 0) {
-                answer += p;
-            }
+        if (p === ')' && --opend > 0) {
+             answer += p;
         }
     })
     return answer;
