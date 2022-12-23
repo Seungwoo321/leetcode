32657ddfe7e50d0f0c42da712413f28922d83f40
Seungwoo321 <seungwoo321@gmail.com>
2022-12-23 23:54:44 +0900

    Time: 86 ms (80.91%), Space: 42.6 MB (67.27%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -3,12 +3,8 @@
  * @param {string} str2
  * @return {string}
  */
-var gcdOfStrings = function(str1, str2, i = str1.length) {
-    if (str1 === str2) return str1
-    if (str1[0] !== str2[0] || i === 1) return '';
-    const x = str1.substring(0, i - 1);
-    const n1 = str1.length / x.length;
-    const n2 = str2.length / x.length;
-    if (x.repeat(n1) === str1 && x.repeat(n2) === str2) return x;
-    return gcdOfStrings(str1, str2, i - 1);
+var gcdOfStrings = function(str1, str2) {
+    if (str1 + str2 !== str2 + str1) return '';
+    const gcd = (a, b) => b > 0 ? gcd(b, a % b) : a;
+    return str1.substring(0, gcd(str1.length, str2.length));
 };
