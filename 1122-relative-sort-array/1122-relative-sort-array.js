2985518e4239eada23aff0a4639370fd59fe8915
Seungwoo321 <seungwoo321@gmail.com>
2022-12-24 23:03:18 +0900

    Time: 72 ms (85.52%), Space: 42.3 MB (70.03%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.js
+++ 1122-relative-sort-array/1122-relative-sort-array.js
@@ -4,17 +4,10 @@
  * @return {number[]}
  */
 var relativeSortArray = function(arr1, arr2) {
-    const mapping = new Map();
-    arr2.forEach((n, i) => mapping.set(n, i));
-    arr1.sort((a, b) => {
-        if (mapping.has(a) && mapping.has(b)) {
-            return mapping.get(a) - mapping.get(b);
-        } else if (mapping.has(a) || mapping.has(b)) {
-            return mapping.has(a) ? -1 : 0;
-        } else {
-            return a - b;
-        }
-        
-    })
-    return arr1;
+    const N = arr2.length;
+    const mapping = arr2.reduce((acc, cur, i) => 
+                                acc.set(cur, i), new Map());
+    return arr1.sort((a, b) => 
+         (mapping.has(a) ? mapping.get(a) : N + a) 
+         - (mapping.has(b) ? mapping.get(b) : N + b));
 };
