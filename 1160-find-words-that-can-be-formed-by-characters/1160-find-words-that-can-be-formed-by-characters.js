a305121458fcce3a212566d75b3dd0c4ee5ccbe5
Seungwoo321 <seungwoo321@gmail.com>
2022-12-25 22:29:07 +0900

    Time: 235 ms (57.77%), Space: 50.3 MB (60.16%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.js
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.js
@@ -4,17 +4,8 @@
  * @return {number}
  */
 var countCharacters = function(words, chars) {
-    let count = 0;
-    const mapping = new Map();
-    [...chars].forEach((c, i) => mapping.set(c, (mapping.get(c) || 0) + 1));
-    words.forEach((word) => {
-        const tmp = new Map();
-        let flag = [...word].every(key => {
-            tmp.set(key, (tmp.get(key) || 0) + 1);
-            return mapping.get(key) >= tmp.get(key);
-        });
-        count += flag ? word.length : 0;
-    });
-
-    return count;
+    return words.reduce((count, word, i) => {
+        for (let c of [...chars]) word = word.replace(c, '');
+        return count + (word.length ? 0 : words[i].length);
+    }, 0);
 };
