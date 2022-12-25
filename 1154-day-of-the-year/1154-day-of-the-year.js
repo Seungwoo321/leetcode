8157e3d462b6592db9d41ed3839087db40b59876
Seungwoo321 <seungwoo321@gmail.com>
2022-12-25 21:25:48 +0900

    Time: 208 ms (85.47%), Space: 51.3 MB (64.96%) - LeetHub

--- 1154-day-of-the-year/1154-day-of-the-year.js
+++ 1154-day-of-the-year/1154-day-of-the-year.js
@@ -3,13 +3,5 @@
  * @return {number}
  */
 var dayOfYear = function(date) {
-    const days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    const year = new Date(date).getFullYear();
-    const leapYear = (year % 4 === 0) && (year % 400 === 0 || year % 100 !== 0);
-    const month = new Date(date).getMonth();
-    let day = new Date(date).getDate() + (month > 1 ? leapYear : 0);
-    for (let i = 0; i < month; i ++) {
-        day += days[i];
-    }
-    return day;
+    return ((new Date(date) - new Date(date.substring(0,4))) + 86400000) / 86400000;
 };
