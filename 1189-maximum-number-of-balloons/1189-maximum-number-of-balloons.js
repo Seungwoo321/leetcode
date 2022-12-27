b27694f9b012f6f7b6422f463699915267277052
Seungwoo321 <seungwoo321@gmail.com>
2022-12-27 19:31:54 +0900

    Time: 65 ms (97.20%), Space: 42.9 MB (81.73%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.js
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.js
@@ -3,12 +3,6 @@
  * @return {number}
  */
 var maxNumberOfBalloons = function(text) {
-    let count = Infinity;
-    const balloon = { b: 1, a: 1, l: 2, o: 2, n: 1 };
-    const mapping = [...text].reduce((acc, cur) => balloon[cur] ? acc.set(cur, (acc.get(cur) || 0) + 1) : acc, new Map());
-    for (let key in balloon) {
-        if (!mapping.has(key)) return 0;
-        count = Math.min(Math.floor(mapping.get(key) / balloon[key]), count);
-    }
-    return count;
+    const balloon = [...text].reduce((acc, cur) => acc.set(cur, (acc.get(cur) || 0) + 1), new Map());
+    return Math.floor(Math.min(balloon.get('b'), balloon.get('a'), balloon.get('l') / 2, balloon.get('o') / 2, balloon.get('n'))) || 0;
 };
