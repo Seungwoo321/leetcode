871e31cd2ee4f7d76603b8680f0003a8ef79afc7
Seungwoo321 <seungwoo321@gmail.com>
2022-12-28 23:30:45 +0900

    Time: 69 ms (86.70%), Space: 41.8 MB (96.06%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.js
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.js
@@ -7,26 +7,13 @@ var tictactoe = function(moves) {
     let b = [[0, 0, 0], [0, 0, 0]];
     let [ad1, ad2] = [0, 0];
     let [bd1, bd2] = [0, 0];
+    
     for (let i = 0; i < moves.length; i ++) {
         const [x, y] = moves[i];
         if (i % 2 === 0) {
-            if (
-                ++a[0][x] === 3 ||
-                ++a[1][y] === 3 ||
-                (x === y && ++ad1 === 3) ||
-                (x + y === 2 && ++ad2 === 3)
-            ) {
-                return 'A';
-            }
+            if (++a[0][x] === 3 || ++a[1][y] === 3 || (x === y && ++ad1 === 3) || (x + y === 2 && ++ad2 === 3)) return 'A';
         } else {
-            if (
-                ++b[0][x] === 3 ||
-                ++b[1][y] === 3 ||
-                (x === y && ++bd1 === 3) ||
-                (x + y === 2 && ++bd2 === 3)
-            ) {
-                return 'B';
-            }
+            if (++b[0][x] === 3 || ++b[1][y] === 3 || (x === y && ++bd1 === 3) || (x + y === 2 && ++bd2 === 3)) return 'B';
         }
     }
     return moves.length === 9 ? 'Draw' : 'Pending';
