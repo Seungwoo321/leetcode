0bd2f1b7b9a37590ee4981f873779f50c31526a7
Seungwoo321 <seungwoo321@gmail.com>
2022-12-28 17:18:05 +0900

    Time: 85 ms (99.22%), Space: 48.1 MB (41.08%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.js
+++ 1260-shift-2d-grid/1260-shift-2d-grid.js
@@ -6,14 +6,13 @@
 var shiftGrid = function(grid, k) {
     let m = grid.length;
     let n = grid[0].length;
-    const vector = new Array(m * n).fill(0)
+    let vector = new Array(m).fill(0).map(v => new Array(n).fill(0))
     for (let i = 0; i < m; i ++) {
         for (let j = 0; j < n; j ++) {
-            vector[((i * n) + j + k) % vector.length] = grid[i][j];
+            let x = Math.floor(i + (j + k) / n) % m;
+            let y = (j + k) % n;
+            vector[x][y] = grid[i][j];
         }
     }
-    for (let l = 0; l < vector.length; l ++) {
-        grid[Math.floor(l / n)][l % n] = vector[l];
-    }
-    return grid;
+    return vector;
 };
