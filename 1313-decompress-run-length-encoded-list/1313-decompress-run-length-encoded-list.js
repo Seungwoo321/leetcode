f6b7f6c2e5422dba0ebc532c14503568901c0de9
Seungwoo321 <seungwoo321@gmail.com>
2022-12-29 21:19:24 +0900

    Time: 100 ms (66.12%), Space: 47.9 MB (12.17%) - LeetHub

--- 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.js
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.js
@@ -5,7 +5,7 @@
 var decompressRLElist = function(nums) {
     let arr = [];
     for (let i = 0; i < nums.length; i += 2) {
-        arr = arr.concat(new Array(nums[i]).fill(nums[i + 1]));
+        arr = [...arr, ...new Array(nums[i]).fill(nums[i + 1])];
     }
     return arr;
 };
