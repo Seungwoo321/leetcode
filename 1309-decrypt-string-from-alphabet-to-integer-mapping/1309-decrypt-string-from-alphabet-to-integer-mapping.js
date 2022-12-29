bff4c202a6060af1997bfed3bb34498e8140048c
Seungwoo321 <seungwoo321@gmail.com>
2022-12-29 20:53:33 +0900

    Time: 54 ms (99.30%), Space: 41.9 MB (84.62%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.js
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.js
@@ -3,9 +3,5 @@
  * @return {string}
  */
 var freqAlphabets = function(s) {    
-    return new Array(26).fill(0).reduce((s, _, i) => {
-        const key = 26 - i < 10 ? 26 - i + '': 26 - i + '#';
-        const char = String.fromCharCode(122 - i);
-        return s.replaceAll(key, char);
-    }, s);
+    return new Array(26).fill(0).reduce((s, _, i) => s.replaceAll(26 - i < 10 ? 26 - i + '': 26 - i + '#', String.fromCharCode(122 - i)), s);
 };
