d40c071640bade591928138c6e7b116a80be2370
Seungwoo321 <seungwoo321@gmail.com>
2022-12-29 19:32:43 +0900

    Time: 59 ms (98.61%), Space: 42.4 MB (74.09%) - LeetHub

---
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var sumZero = function(n, arr = []) {
+    if (n % 2 === 1) arr.push(0);
+    let m = n % 2 === 1 ? n - 1 : n;
+    for (let i = 0; i < m / 2; i ++) {
+        arr.push(i + 1);
+        arr.push(- i - 1);
+    }
+    return arr;
+};
