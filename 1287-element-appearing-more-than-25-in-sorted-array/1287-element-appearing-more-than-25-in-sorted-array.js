ec44d88cf92c1dbb2fcb0d2ae48d46163e4c2f3e
Seungwoo321 <seungwoo321@gmail.com>
2022-12-29 17:17:20 +0900

    Time: 68 ms (91.89%), Space: 42.1 MB (98.65%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.js
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.js
@@ -3,17 +3,11 @@
  * @return {number}
  */
 var findSpecialInteger = function(arr) {
-    if (arr.length === 1) return arr[0];
-    const maxFreq = Math.floor(arr.length * 0.25);
-    let count = 0;
-    for (let i = 0; i < arr.length - 1; i ++) {
-        if (arr[i] === arr[i + 1]) {
-            count ++;
-        } else {
-            count = 0;
-        }
-        if (count >= maxFreq) {
+    const maxFreq = Math.floor(arr.length / 4);
+    for (let i = 0; i < arr.length; i ++) {
+        if (arr[i] === arr[i + maxFreq]) {
             return arr[i];
         }
     }
+    return -1;
 };
