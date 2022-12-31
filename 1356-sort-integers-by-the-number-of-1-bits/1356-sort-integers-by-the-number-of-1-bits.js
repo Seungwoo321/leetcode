0257fff5271be2ba9579899edb73b31fba0a8fcb
Seungwoo321 <seungwoo321@gmail.com>
2022-12-31 21:19:45 +0900

    Time: 177 ms (31.17%), Space: 48.1 MB (26.91%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var sortByBits = function(arr) {
+    return arr.sort((a, b) => {
+        const a1 = [...a.toString(2)].filter(v => v === '1').length;
+        const b1 = [...b.toString(2)].filter(v => v === '1').length;
+        return a1 - b1 === 0 ? a - b : a1 - b1;
+    });
+};
