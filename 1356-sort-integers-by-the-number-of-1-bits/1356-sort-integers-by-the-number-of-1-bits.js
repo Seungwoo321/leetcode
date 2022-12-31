df469ad681ef4b06151bfc2a88db590d376a9cb6
Seungwoo321 <seungwoo321@gmail.com>
2022-12-31 21:44:05 +0900

    Time: 179 ms (30.94%), Space: 48 MB (30.94%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.js
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.js
@@ -3,14 +3,9 @@
  * @return {number[]}
  */
 var sortByBits = function(arr) {
-    const numberOf1Bits = (n) => {
-        let count = 0;
-        while (n > 0) {
-            count += n & 1;
-            n >>= 1;
-        }
-        return count;
-    }    
-    return arr.sort((a, b) => numberOf1Bits(a) - numberOf1Bits(b) || a - b); 
-
+    return arr.sort((a, b) => {
+        const a1 = [...a.toString(2)].filter(v => v === '1').length;
+        const b1 = [...b.toString(2)].filter(v => v === '1').length;
+        return a1 - b1 || a - b;
+    });
 };
