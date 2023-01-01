9319a74fcfad890d34ab52c54605762763018bbf
Seungwoo321 <seungwoo321@gmail.com>
2023-01-01 19:52:37 +0900

    Time: 79 ms (93.86%), Space: 44.5 MB (37.39%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
@@ -3,8 +3,7 @@
  * @return {number[]}
  */
 var smallerNumbersThanCurrent = function(nums) {
-    const max = Math.max(...nums)
-    const counts = new Array(max + 1).fill(0)
+    const counts = new Array(100).fill(0)
     nums.forEach(n => counts[n]++);
     counts.forEach((_, i) => counts[i] += (counts[i - 1] || 0));
     return nums.map((n, i) => counts[n - 1] || 0);
