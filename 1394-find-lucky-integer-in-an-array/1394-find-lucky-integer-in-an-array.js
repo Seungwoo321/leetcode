8a1abd3a90f1a670dc9f9b553283e19aa59390af
Seungwoo321 <seungwoo321@gmail.com>
2023-01-04 19:38:37 +0900

    Time: 85 ms (57.33%), Space: 44.4 MB (28.28%) - LeetHub

--- 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.js
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.js
@@ -3,10 +3,6 @@
  * @return {number}
  */
 var findLucky = function(arr) {
-    let lucky = -1;
     const counting = arr.reduce((acc, cur) => acc.set(cur, (acc.get(cur) || 0) + 1), new Map());
-    for (let [key, value]  of counting) {
-        if (key === value && value > lucky) lucky = value;
-    }
-    return lucky;
+    return Math.max(...arr.filter(n => n === counting.get(n)), -1);
 };
