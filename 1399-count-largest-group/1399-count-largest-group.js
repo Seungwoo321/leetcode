7a50d965d5555730aad993adb37a2417f576eaa1
Seungwoo321 <seungwoo321@gmail.com>
2023-01-05 16:38:46 +0900

    Time: 153 ms (23.08%), Space: 46 MB (58.46%) - LeetHub

--- 1399-count-largest-group/1399-count-largest-group.js
+++ 1399-count-largest-group/1399-count-largest-group.js
@@ -3,12 +3,22 @@
  * @return {number}
  */
 var countLargestGroup = function(n) {
-    const digitSum = (n) => n === 0 ? 0 : (n % 10) + digitSum(Math.floor(n / 10));
+    const digitSum = n => {
+        let sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n = Math.floor(n / 10);
+        }
+        return sum;
+    }
+    // const digitSum = (n) => n === 0 ? 0 : (n % 10) + digitSum(Math.floor(n / 10));
     const counts = new Array(37).fill(0);
     let max = 0;
     for (let i = 1; i <= n; i++) {
         counts[digitSum(i)]++
         max = Math.max(max, counts[digitSum(i)]);
     }
+    console.log(counts);
+    console.log(max)
     return counts.filter(c => c === max).length;
 };
