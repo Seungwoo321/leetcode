36578135e61b6a76a5acb2186a07e0a7ac036b6e
Seungwoo321 <seungwoo321@gmail.com>
2023-01-05 23:42:55 +0900

    Time: 109 ms (55.00%), Space: 44.1 MB (87.86%) - LeetHub

--- 1403-minimum-subsequence-in-non-increasing-order/1403-minimum-subsequence-in-non-increasing-order.js
+++ 1403-minimum-subsequence-in-non-increasing-order/1403-minimum-subsequence-in-non-increasing-order.js
@@ -3,15 +3,14 @@
  * @return {number[]}
  */
 var minSubsequence = function(nums) {
-    nums.sort((a, b) => b - a);
-    const total = nums.reduce((s, n) => s + n, 0);
+    const total = nums.sort((a, b) => b - a).reduce((s, n) => s + n, 0);
     const arr = [];
     let sum = 0;
     for (let i = 0; i < nums.length; i ++) {
-        if (sum <= total - sum) {
-            sum += nums[i];
-            arr.push(nums[i]);
-        }
+        if (sum > total - sum) break;
+        sum += nums[i];
+        arr.push(nums[i]);
+       
     }
     return arr;
 };
